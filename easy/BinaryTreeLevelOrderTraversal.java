0364ee80f261f1f0b36fac2587970f89a2199746
altuc <zhou.hanj@husky.neu.edu>
2014-11-13 12:57:51 -0500

    committing leetcode binarytreelevelordertraversal

---
+++ easy/BinaryTreeLevelOrderTraversal.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<List<Integer>> levelOrder(TreeNode root) {
+        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(root == null) {
+            return res;
+        }
+        Queue<TreeNode> q = new LinkedList<TreeNode>();
+        q.add(root);
+        while(!q.isEmpty()) {
+            ArrayList<Integer> level = new ArrayList<Integer>();
+            ArrayList<TreeNode> nodeList = new ArrayList<TreeNode>();
+            while(!q.isEmpty()) {
+                nodeList.add(q.remove());
+            }
+            for(TreeNode n : nodeList) {
+                level.add(n.val);
+                if(n.left != null) {
+                    q.add(n.left);
+                }
+                if(n.right != null) {
+                    q.add(n.right);
+                }
+            }
+            res.add(level);
+        }
+        return res;
+    }
+}
