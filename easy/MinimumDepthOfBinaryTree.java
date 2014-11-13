2e86dadc00388437597eae18f2872075e2b3ba28
altuc <zhou.hanj@husky.neu.edu>
2014-11-12 23:48:51 -0500

    committing leetcode minimumdepthofbinarytree

---
+++ easy/MinimumDepthOfBinaryTree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution1 {
+    public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return minDepthHelper(root.left, root.right, 1);
+    }
+    
+    public int minDepthHelper(TreeNode l, TreeNode r, int depth) {
+        if(l == null && r == null) {
+            return depth;
+        } else if(l == null && r != null) {
+            return minDepthHelper(r.left, r.right, depth + 1);
+        } else if(l != null && r == null) {
+            return minDepthHelper(l.left, l.right, depth + 1);
+        } else {
+            return Math.min(minDepthHelper(l.left, l.right, depth + 1), minDepthHelper(r.left, r.right, depth + 1));
+        }
+    }
+}
+
+public class Solution2 {
+    public int minDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return minDepthHelper(root, 0);
+    }
+    
+    public int minDepthHelper(TreeNode n, int depth) {
+        depth++;
+        if(n == null) {
+            return Integer.MAX_VALUE;
+        }
+        if(n.left == null && n.right == null) {
+            return depth;
+        }
+        return Math.min(minDepthHelper(n.left, depth), minDepthHelper(n.right, depth));
+    }
+}
