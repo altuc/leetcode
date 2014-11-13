5010d8c2f3f04f38f43ca9db0c6cf75931b351c7
altuc <zhou.hanj@husky.neu.edu>
2014-11-13 11:22:35 -0500

    committing leetcode maximumdepthofbinarytree

---
+++ easy/MaximumDepthOfBinaryTree.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for binary tree
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution1 {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return maxDepthHelper(root.left, root.right, 1);
+    }
+    
+    public int maxDepthHelper(TreeNode l, TreeNode r, int depth) {
+        if(l == null && r == null) {
+            return depth;
+        } else if(l == null && r != null) {
+            return maxDepthHelper(r.left, r.right, depth + 1);
+        } else if(l != null && r == null) {
+            return maxDepthHelper(l.left, l.right, depth + 1);
+        } else {
+            return Math.max(maxDepthHelper(l.left, l.right, depth + 1), maxDepthHelper(r.left, r.right, depth + 1));
+        }
+    }
+}
+
+public class Solution2 {
+    public int maxDepth(TreeNode root) {
+        if(root == null) {
+            return 0;
+        }
+        return maxDepthHelper(root, 0);
+    }
+    
+    public int maxDepthHelper(TreeNode n, int depth) {
+        depth++;
+        if(n == null) {
+            return 0;
+        }
+        if(n.left == null && n.right == null) {
+            return depth;
+        }
+        return Math.max(maxDepthHelper(n.left, depth), maxDepthHelper(n.right, depth));
+    }
+}
