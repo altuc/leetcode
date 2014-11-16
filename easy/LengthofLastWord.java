f5ead928f3409b6cbb6d24a465bb56cd97c48c21
altuc <zhou.hanj@husky.neu.edu>
2014-11-16 15:26:55 -0500

    committing leetcode lengthoflastword

---
+++ easy/LengthofLastWord.java
@@ -0,0 +1,36 @@
+/* characters are only alphabetical in last word */
+public class Solution1 {
+    public int lengthOfLastWord(String s) {
+        String str = s.toLowerCase().trim();
+        if(str.isEmpty() || s == null) {
+            return 0;
+        }
+        String[] strs = str.split(" ");
+        for(char c : strs[strs.length - 1].toCharArray()) {
+            if(c > 'z' || c < 'a') {
+                return 0;
+            }
+        }
+        return strs[strs.length - 1].length();
+    }
+}
+
+/* characters are not only alphabetical in last word */
+public class Solution2 {
+    public int lengthOfLastWord(String s) {
+        if(s == null || s.length() == 0) {
+            return 0;
+        }
+        int end = s.length() - 1;
+        int count = 0;
+        for (int i = end; i > -1; i--) {
+            if (s.charAt(i) != ' ') {
+                count++;
+            }
+            if (s.charAt(i) == ' ' && count != 0) {
+                return count;
+            }
+        }
+        return count;
+    }
+}
