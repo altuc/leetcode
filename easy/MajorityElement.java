313ad7279fa5b117f663caa719728c66721c212d
beeacoder <zhou.hanj@husky.neu.edu>
2016-01-05 17:19:36 -0500

    committing leetcode majorityelement

---
+++ easy/MajorityElement.java
@@ -0,0 +1,25 @@
+public class Solution1 {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        return nums[nums.length/2];
+    }
+}
+
+public class Solution2 {
+    public int majorityElement(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int res = 0;
+        for (int num: nums) {
+            if (!map.containsKey(num)){
+                map.put(num, 1);
+            } else {
+                map.put(num, map.get(num) + 1);
+            }
+            if (map.get(num) > nums.length/2) {
+                res = num;
+                break;
+            }
+        }
+        return res;
+    }
+}
