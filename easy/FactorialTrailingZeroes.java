c9f711bf37889d71b981e3a442b30ec92e5f0775
beeacoder <zhou.hanj@husky.neu.edu>
2016-01-06 23:41:28 -0500

    committing leetcode factorialtrailingzeroes

---
+++ easy/FactorialTrailingZeroes.java
@@ -0,0 +1,20 @@
+public class Solution1 {
+    public int trailingZeroes(int n) {
+        int res = 0;
+        while(n > 1) {
+            n /= 5;
+            res += n;
+        }
+        return res;
+    }
+}
+
+public class Solution2 {
+    public int trailingZeroes(int n) {
+        if(n < 5) {
+            return 0;
+        } else {
+            return n/5 + trailingZeroes(n/5);
+        }
+    }
+}
