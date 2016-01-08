bdaba06c2ccd0e8353fe6a4a2f962c243c58769f
beeacoder <zhou.hanj@husky.neu.edu>
2016-01-08 17:24:53 -0500

    Update ValidParentheses.java

--- easy/ValidParentheses.java
+++ easy/ValidParentheses.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public boolean isValid(String s) {
         if(s.isEmpty()) {
             return true;
@@ -19,5 +19,30 @@ public class Solution {
             }
         }
         return sta.size() == 0;
+    }
+}
+
+public class Solution2 {
+    public boolean isValid(String s) {
+        if(s.isEmpty() || s == null) {
+            return true;
+        }
+        if(s.charAt(0) == ')' || s.charAt(0) == ']' || s.charAt(0) == '}' || s.length() == 1) {
+            return false;
+        }
+        Stack<Character> sta = new Stack<>();
+        for(char c : s.toCharArray()) {
+            if(sta.empty()) {
+                sta.push(c);
+            } else {
+                if((c == ')' && sta.pop() != '(') || (c == ']' && sta.pop() != '[') || (c == '}' && sta.pop() != '{')) {
+                    return false;
+                } 
+                if(c == '(' || c == '[' || c == '{') {
+                    sta.push(c);
+                }
+            }
+        }
+        return sta.empty();
     }
 }
