6099ff1df7851ccf6011527cc6ba34b58370b41e
beeacoder <zhou.hanj@husky.neu.edu>
2016-01-09 21:04:35 -0500

    committing leetcode powerofthree

---
+++ easy/PowerOfThree.java
@@ -0,0 +1,27 @@
+public class Solution1 {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        while(n % 3 == 0) {
+            n /= 3;
+        }
+        return n == 1;
+    }
+}
+
+public class Solution2 {
+    public boolean isPowerOfThree(int n) {
+        if(n <= 0) {
+            return false;
+        }
+        if(n == 1) {
+            return true;
+        }
+        if(n % 3 == 0) {
+            return isPowerOfThree(n/3);
+        } else {
+            return false;
+        }
+    }
+}
