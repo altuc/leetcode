7a0d97e3766dd4ea8fbca9fb2134772c0ea72b46
beeacoder <zhou.hanj@husky.neu.edu>
2016-01-10 02:26:32 -0500

    committing leetcode wordpattern

---
+++ easy/WordPattern.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public boolean wordPattern(String pattern, String str) {
+        String[] words = str.trim().split(" ");
+        if(pattern.length() != words.length) {
+            return false;
+        }
+        HashMap<Character, String> map = new HashMap<>();
+        for(int i = 0; i < pattern.length(); i++) {
+            if(!map.containsKey(pattern.charAt(i))) {
+                if(map.containsValue(words[i])) {
+                    return false;
+                }
+                map.put(pattern.charAt(i), words[i]);
+            } else {
+                if(!map.get(pattern.charAt(i)).equals(words[i])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
