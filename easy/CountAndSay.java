6806991cddc99a362ce1efc6bdca43ffe003f508
beeacoder <zhou.hanj@husky.neu.edu>
2016-01-11 16:18:30 -0500

    Update CountAndSay.java

--- easy/CountAndSay.java
+++ easy/CountAndSay.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public String countAndSay(int n) {
         String[] str = new String[n];
         str[0] = "1";
@@ -16,5 +16,25 @@ public class Solution {
             str[i] = str[i] + Integer.toString(count) + Character.toString(str[i-1].charAt(str[i-1].length() - 1));
         }
         return str[n-1];
+    }
+}
+
+public class Solution2 {
+    public String countAndSay(int n) {
+        String cur = "1";
+        while(n > 1) {
+            StringBuilder sb = new StringBuilder();
+            for(int i = 0; i < cur.length(); i++) {
+                int count = 1;
+                while((i+1) < cur.length() && cur.charAt(i) == cur.charAt(i+1)) {
+                    count++;
+                    i++;
+                }
+                sb = sb.append(Integer.toString(count)).append(Character.toString(cur.charAt(i)));
+            }
+            cur = sb.toString();
+            n--;
+        }
+        return cur;
     }
 }
