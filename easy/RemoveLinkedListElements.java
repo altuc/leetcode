94d6ce11207ebe4b23dac843cc00870903678a25
beeacoder <zhou.hanj@husky.neu.edu>
2016-01-11 13:10:05 -0500

    committing leetcode removelinkedlistelement

---
+++ easy/RemoveLinkedListElements.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode sentinel = new ListNode(0);
+        sentinel.next = head;
+        ListNode temp = sentinel;
+        while(temp.next != null) {
+            if(temp.next.val == val) {
+                temp.next = temp.next.next;
+            } else {
+                temp = temp.next;
+            }
+        }
+        return sentinel.next;
+    }
+}
