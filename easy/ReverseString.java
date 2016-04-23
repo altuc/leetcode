3dad70433be50ce3975866809db1929b025b8d07
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-22 21:19:26 -0400

    Update ReverseString.java

--- easy/ReverseString.java
+++ easy/ReverseString.java
@@ -1,6 +1,6 @@
 public class Solution {
     public String reverse(String str) {
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 	for(int i = str.length() - 1; i >= 0; i--) {
 	    sb.append(str.charAt(i));
 	}
