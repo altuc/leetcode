fe016497c63aafef052d1bee57d366eeffd14cfd
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-22 22:13:46 -0400

    Update PowerOfFour.java

--- easy/PowerOfFour.java
+++ easy/PowerOfFour.java
@@ -16,3 +16,9 @@ public class Solution2 {
         return ((num & (num - 1)) == 0) && (num > 0) && ((num & 0x55555555) == num);
     }
 }
+
+public class Solution3 {
+    public boolean isPowerOfFour(int num) {
+        return (Math.log10(num)/Math.log10(4)) % 1 == 0;
+    }
+}
