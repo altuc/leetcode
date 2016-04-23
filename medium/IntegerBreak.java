04fe541be37d93591b4e2f66593568ff97649934
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-23 16:58:19 -0400

    committing leetcode integerbreak

---
+++ medium/IntegerBreak.java
@@ -0,0 +1,17 @@
+public class Solution {
+    public int integerBreak(int n) {
+        if(n == 2) {
+            return 1;
+        }
+        if(n == 3) {
+            return 2;
+        }
+        int res = 1;
+        while(n - 3 > 1) {
+            res *= 3;
+            n -= 3;
+        }
+        res *= n;
+        return res;
+    }
+}
