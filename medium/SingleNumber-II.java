3f3607ada43a6f094a5c0f4f606a5a0b9aee3643
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-24 00:47:21 -0400

    Update SingleNumber-II.java

--- medium/SingleNumber-II.java
+++ medium/SingleNumber-II.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public int singleNumber(int[] nums) {
         int res = 0;
         for(int i = 0; i < 32; i++) {
@@ -9,5 +9,23 @@ public class Solution {
             res |= (sum % 3) << i;
         }
         return res;
+    }
+}
+
+public class Solution2 {
+    public int singleNumber(int[] nums) {
+        // Java integer size is 32 bit 
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            int mask = 1 << i;
+            int sum = 0;
+            for (int j = 0; j < nums.length; j++) {
+                if ((nums[j] & mask) != 0)
+                    sum++;
+            }
+            if (sum % 3 != 0)
+                result |= (sum % 3) << i;
+        }
+        return result;
     }
 }
