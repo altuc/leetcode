bdc6fe8753b983df06705ad7dddbb94a07fe11af
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-23 23:56:07 -0400

    committing leetcode binarytreeinordertraversal

---
+++ medium/BinaryTreeInorderTraversal.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution1 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if(root == null) {
+            return res;
+        }
+        Stack<TreeNode> s = new Stack<TreeNode>();
+        TreeNode n = root;
+        while(!s.empty() || n != null) {
+            if(n != null) {
+                s.push(n);
+                n = n.left;
+            } else {
+                n = s.pop();
+                res.add(n.val);
+                n = n.right;
+            }
+        }
+        return res;
+    }
+}
+
+public class Solution2 {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> res = new ArrayList<Integer>();
+        if(root == null) {
+            return res;
+        }
+        res.addAll(inorderTraversal(root.left));
+        res.add(root.val);
+        res.addAll(inorderTraversal(root.right));
+        return res;
+    }
+}
