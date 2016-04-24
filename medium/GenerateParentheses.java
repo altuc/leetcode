b57462f8430f5254294014e689aadf59be433f78
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-24 14:32:36 -0400

    committing leetcode generateparentheses

---
+++ medium/GenerateParentheses.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public List<String> generateParenthesis(int n) {
+        ArrayList<String> res = new ArrayList<String>();
+        if(n <= 0) {
+            return res;
+        }
+        generateParenthesisHelper(res, "", n, n);
+        return res;
+    }
+    
+    public void generateParenthesisHelper(ArrayList<String> res, String current, int left, int right) {
+        if(left > right || left < 0 || right < 0) {
+            return;
+        }
+        if(left == 0 && right == 0) {
+            res.add(current);
+            return;
+        }
+        generateParenthesisHelper(res, current + "(", left - 1, right);
+        generateParenthesisHelper(res, current + ")", left, right - 1);
+    }
+}
