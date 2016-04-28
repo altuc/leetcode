a781e06a999904da972a6e075aee4e3283ebb21e
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-28 00:36:21 -0400

    committing leetcode convertsortedlisttobst

---
+++ medium/ConvertSortedListToBinarySearchTree.java
@@ -0,0 +1,39 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public TreeNode sortedListToBST(ListNode head) {
+        if(head == null) {
+            return null;
+        }
+        if(head.next == null) {
+            return new TreeNode(head.val);
+        }
+        ListNode slow = head;
+        ListNode fast = head.next.next;
+        while(fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        TreeNode root = new TreeNode(slow.next.val);
+        root.right = sortedListToBST(slow.next.next);
+        slow.next = null;
+        root.left = sortedListToBST(head);
+        
+        return root;
+    }
+}
