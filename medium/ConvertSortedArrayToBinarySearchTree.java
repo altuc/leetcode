640720aeed6b831a98035af87642e8e0076c3224
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-27 22:50:55 -0400

    committing leetcode convertsortedarraytobst

---
+++ medium/ConvertSortedArrayToBinarySearchTree.java
@@ -0,0 +1,40 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution1 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return null;
+        }
+        return sortedArrayToBSTHelper(nums, 0, nums.length);
+    }
+    
+    public TreeNode sortedArrayToBSTHelper(int[] nums, int start, int end) {
+        if(start >= end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBSTHelper(nums, start, mid);
+        node.right = sortedArrayToBSTHelper(nums, mid + 1, end);
+        return node;
+    }
+}
+
+public class Solution2 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if (nums.length == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(nums[nums.length / 2]);
+        root.left = sortedArrayToBST(Arrays.copyOfRange(nums, 0, nums.length / 2));
+        root.right = sortedArrayToBST(Arrays.copyOfRange(nums, nums.length / 2 + 1, nums.length));
+        return root;
+    }
+}
