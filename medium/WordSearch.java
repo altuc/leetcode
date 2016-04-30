d03e16bbe52afe729e343474f3f866aed3a6b1b2
beeacoder <zhou.hanj@husky.neu.edu>
2016-04-30 14:20:01 -0400

    committing leetcode wordsearch

---
+++ medium/WordSearch.java
@@ -0,0 +1,38 @@
+public class Solution {
+    public boolean exist(char[][] board, String word) {
+        if(board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        if(word == null || word.length() == 0 || word.length() > board.length * board[0].length) {
+            return false;
+        }
+        boolean[][] visited = new boolean[board.length][board[0].length];
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(existHelper(board, word, i, j, 0, visited)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+    
+    public boolean existHelper(char[][] board, String word, int top, int left, int start, boolean[][] visited) {
+        if(start == word.length()) {
+            return true;
+        }
+        if(top < 0 || top == board.length || left < 0 || left == board[0].length) {
+            return false;
+        }
+        if(visited[top][left] || board[top][left] != word.charAt(start)) {
+            return false;
+        }
+        visited[top][left] = true;
+        boolean res = existHelper(board, word, top + 1, left, start + 1, visited)
+                      || existHelper(board, word, top - 1, left, start + 1, visited)
+                      || existHelper(board, word, top, left + 1, start + 1, visited)
+                      || existHelper(board, word, top, left - 1, start + 1, visited);
+        visited[top][left] = false;
+        return res;
+    }
+}
