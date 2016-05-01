e49288cd59a5d21ed461638f2087cb1a87600713
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-01 19:56:14 -0400

    Update SortColors.java

--- medium/SortColors.java
+++ medium/SortColors.java
@@ -2,16 +2,12 @@ public class Solution {
     public void sortColors(int[] nums) {
         int red = 0;
         int white = 0;
-        int blue = 0;
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] == 0) {
                 red++;
             }
             if(nums[i] == 1) {
                 white++;
-            }
-            if(nums[i] == 2) {
-                blue++;
             }
         }
         for(int i = 0; i < nums.length; i++) {
