eddd51c8d92117dea25247e7929e96a8d489d6ff
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-01 19:52:55 -0400

    committing leetcode sortcolors

---
+++ medium/SortColors.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public void sortColors(int[] nums) {
+        int red = 0;
+        int white = 0;
+        int blue = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] == 0) {
+                red++;
+            }
+            if(nums[i] == 1) {
+                white++;
+            }
+            if(nums[i] == 2) {
+                blue++;
+            }
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(i < red) {
+                nums[i] = 0;
+            }
+            if(i >= red && i < white + red) {
+                nums[i] = 1;
+            }
+            if(i >= white + red) {
+                nums[i] = 2;
+            }
+        }
+    }
+}
