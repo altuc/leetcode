f95b5858234578b942edf237ee3ea68902251c07
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-01 15:00:13 -0400

    Update ValidPalindrome.java

--- easy/ValidPalindrome.java
+++ easy/ValidPalindrome.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public boolean isPalindrome(String s) {
         if(s.isEmpty()) {
             return true;
@@ -15,6 +15,36 @@ public class Solution {
                 if(c != sta.pop()) {
                     return false;
                 }
+            }
+        }
+        return true;
+    }
+}
+
+public class Solution2 {
+    public boolean isPalindrome(String s) {
+        if(s == null || s.isEmpty()) {
+            return true;
+        }
+        String str = s.toLowerCase();
+        int start = 0;
+        int end = s.length() - 1;
+        while(start < end) {
+            char left = str.charAt(start);
+            char right = str.charAt(end);
+            if(((left >= 'a' && left <= 'z') || (left >= '0' && left <= '9')) && ((right >= 'a' && right <= 'z') || (right >= '0' && right <= '9'))) {
+                if(left != right) {
+                    return false;
+                }
+                start++;
+                end--;
+            } else if(!((left >= 'a' && left <= 'z') || (left >= '0' && left <= '9')) && ((right >= 'a' && right <= 'z') || (right >= '0' && right <= '9'))) {
+                start++;
+            } else if(((left >= 'a' && left <= 'z') || (left >= '0' && left <= '9')) && !((right >= 'a' && right <= 'z') || (right >= '0' && right <= '9'))) {
+                end--;
+            } else {
+                start++;
+                end--;
             }
         }
         return true;
