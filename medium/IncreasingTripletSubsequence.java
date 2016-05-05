960a832a9839019457476c05e23b3dc316bd2272
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-04 23:50:56 -0400

    Update IncreasingTripletSubsequence.java

--- medium/IncreasingTripletSubsequence.java
+++ medium/IncreasingTripletSubsequence.java
@@ -3,18 +3,14 @@ public class Solution {
         if(nums == null || nums.length < 3) {
             return false;
         }
-        int[] res = new int[nums.length];
-        for(int i = 0; i < nums.length; i++) {
-            int temp = i;
-            for(int j = i + 1; j < nums.length; j++) {
-                if(nums[j] > nums[temp]) {
-                    res[i]++;
-                    temp = j;
-                }
-            }
-        }
-        for(int i = 0; i < res.length; i++) {
-            if(res[i] >= 2) {
+        int i = Integer.MAX_VALUE;
+        int j = Integer.MAX_VALUE;
+        for(int n : nums) {
+            if(n <= i) {
+                i = n;
+            } else if(n <= j) {
+                j = n;
+            } else {
                 return true;
             }
         }
