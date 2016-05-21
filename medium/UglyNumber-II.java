77349272d3acb12af4d977629c2762cd13c746cb
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-21 16:46:08 -0400

    committing leetcode uglynumber-ii

---
+++ medium/UglyNumber-II.java
@@ -0,0 +1,29 @@
+public class Solution {
+    public int nthUglyNumber(int n) {
+        int[] res = new int[n];
+        res[0] = 1;
+        int index2 = 0;
+        int index3 = 0;
+        int index5 = 0;
+        int factor2 = 2;
+        int factor3 = 3;
+        int factor5 = 5;
+        for(int i = 1; i < n; i++) {
+            int min = Math.min(Math.min(factor2, factor3), factor5);
+            res[i] = min;
+            if(factor2 == min) {
+                index2++;
+                factor2 = 2 * res[index2];
+            }
+            if(factor3 == min) {
+                index3++;
+                factor3 = 3 * res[index3];
+            }
+            if(factor5 == min) {
+                index5++;
+                factor5 = 5 * res[index5];
+            }
+        }
+        return res[n - 1];
+    }
+}
