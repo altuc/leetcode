904fe9c9e89ea56da6f77ae12fd2fa83cd16964b
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-22 00:06:27 -0400

    committing leetcode groupanagrams

---
+++ medium/GroupAnagrams.java
@@ -0,0 +1,23 @@
+public class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        List<List<String>> res = new ArrayList<List<String>>();
+        if(strs == null || strs.length == 0) {
+            return res;
+        }
+        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
+        Arrays.sort(strs);
+        for(String str : strs) {
+            char[] arr = str.toCharArray();
+            Arrays.sort(arr);
+            String key = String.valueOf(arr);
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<String>());
+            } 
+            map.get(key).add(str);
+        }
+        for(Map.Entry<String, List<String>> entry : map.entrySet()) {
+            res.add(entry.getValue());
+        }
+        return res;
+    }
+}
