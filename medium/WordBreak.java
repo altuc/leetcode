0fad1d1c231c12d2dab9280e3279a07925b19a8e
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-24 00:07:33 -0400

    committing leetcode wordbreak

---
+++ medium/WordBreak.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public boolean wordBreak(String s, Set<String> wordDict) {
+        boolean[] res = new boolean[s.length() + 1];
+        res[0] = true;
+        for(int i = 1; i <= s.length(); i++) {
+            for(int j = 0; j < i; j++) {
+                if(res[j] && wordDict.contains(s.substring(j, i))) {
+                    res[i] = true;
+                    break;
+                }
+            }
+        }
+        return res[s.length()];
+    }
+}
