e4858cf1844ffbdb511a014dcd306422dbb4a7c9
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-26 17:14:43 -0400

    Update MoveZeros.java

--- easy/MoveZeros.java
+++ easy/MoveZeros.java
@@ -31,3 +31,20 @@ public class Solution2 {
         }
     }
 }
+
+public class Solution3 {
+    public void moveZeroes(int[] nums) {
+        if(nums == null || nums.length <= 1) {
+            return;
+        }
+        int firstZero = 0;
+        for(int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0) {
+                int temp = nums[i];
+                nums[i] = 0;
+                nums[firstZero] = temp;
+                firstZero++;
+            }
+        }
+    }
+}
