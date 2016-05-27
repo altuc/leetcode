a3ffa503f5a8b7b8c0af0baa93f849ae013c1499
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-26 22:59:22 -0400

    Update NumberOf1Bits.java

--- easy/NumberOf1Bits.java
+++ easy/NumberOf1Bits.java
@@ -3,10 +3,8 @@ public class Solution1 {
     public int hammingWeight(int n) {
         int count = 0;
         for(int i = 0; i < 32; i++) {
-            if((n & 1) == 1) {
-                count++;
-            }
-            n = n >> 1;
+            count += n & 1;
+            n >>= 1;
         }
         return count;
     }
