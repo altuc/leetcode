219bab6ca23a3dbd063e7d328ca4bf2ea46cfef8
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-27 13:41:10 -0400

    Update HouseRobber.java

--- easy/HouseRobber.java
+++ easy/HouseRobber.java
@@ -13,7 +13,7 @@ public class Solution {
         res[0] = nums[0];
         res[1] = Math.max(nums[0], nums[1]);
         for(int i = 2; i < nums.length; i++) {
-            res[i] = Math.max((nums[i] + res[i-2]), res[i-1]);
+            res[i] = Math.max((nums[i] + res[i - 2]), res[i - 1]);
         }
         return res[nums.length - 1];
     }
