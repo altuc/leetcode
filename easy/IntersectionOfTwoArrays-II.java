355a4b4074b246378b4a2bc0487a902227fd33c6
beeacoder <zhou.hanj@husky.neu.edu>
2016-05-26 22:02:07 -0400

    committing leetcode intersectionoftwoarrays-ii

---
+++ easy/IntersectionOfTwoArrays-II.java
@@ -0,0 +1,26 @@
+public class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
+        ArrayList<Integer> interList = new ArrayList<Integer>();
+        for(int num : nums1) {
+            if(map.containsKey(num)) {
+                map.put(num, map.get(num) + 1);
+            } else {
+                map.put(num, 1);
+            }
+        }
+        for(int num : nums2) {
+            if(map.containsKey(num) && map.get(num) > 0) {
+                interList.add(num);
+                map.put(num, map.get(num) - 1);
+            }
+        }
+        int[] res = new int[interList.size()];
+        int index = 0;
+        for(int num : interList) {
+            res[index] = num;
+            index++;
+        }
+        return res;
+    }
+}
