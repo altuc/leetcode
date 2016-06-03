b6e9818c64f9fbd2c31b86f519015e952d67486e
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-02 22:17:32 -0400

    Update SearchInsertPosition.java

--- medium/SearchInsertPosition.java
+++ medium/SearchInsertPosition.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public int searchInsert(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] == target || nums[i] > target) {
@@ -6,5 +6,26 @@ public class Solution {
             }
         }
         return nums.length;
+    }
+}
+
+public class Solution2 {
+    public int searchInsert(int[] nums, int target) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while(start <= end) {
+            int mid = (start + end) / 2;
+            if(nums[mid] < target) {
+                start = mid + 1;
+            } else if(nums[mid] > target) {
+                end = mid - 1;
+            } else {
+                return mid;
+            }
+        }
+        return start;
     }
 }
