e5e75d6589c99751e5803b32b1ccd4374f412ac9
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-03 15:16:17 -0400

    Update AddBinary.java

--- easy/AddBinary.java
+++ easy/AddBinary.java
@@ -118,3 +118,35 @@ public class Solution2 {
 		return sb.toString();
 	}
 }
+
+public class Solution3 {
+    public String addBinary(String a, String b) {
+        if(a == null || a.isEmpty()) {
+            return b;
+        }
+        if(b == null || b.isEmpty()) {
+            return a;
+        }
+        int pa = a.length() - 1;
+        int pb = b.length() - 1;
+        int carry = 0;
+        StringBuilder sb = new StringBuilder();
+        while(pa >= 0 || pb >= 0) {
+            int sum = carry;
+            if(pa >= 0) {
+                sum += a.charAt(pa) - '0';
+                pa--;
+            }
+            if(pb >= 0) {
+                sum += b.charAt(pb) - '0';
+                pb--;
+            }
+            sb.insert(0, sum % 2);
+            carry = sum >= 2 ? 1 : 0;
+        }
+        if(carry == 1) {
+            sb.insert(0, 1);
+        }
+        return sb.toString();
+    }
+}
