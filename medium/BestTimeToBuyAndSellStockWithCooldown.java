57eb661ffb74b2233e15a42d47ca05e20646b9ff
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-04 17:20:03 -0400

    Update BestTimeToBuyAndSellStockWithCooldown.java

--- medium/BestTimeToBuyAndSellStockWithCooldown.java
+++ medium/BestTimeToBuyAndSellStockWithCooldown.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public int maxProfit(int[] prices) {
         if(prices == null || prices.length <= 1) {
             return 0;
@@ -11,5 +11,22 @@ public class Solution {
             sell = Math.max(hold + prices[i], noHold);
         }
         return sell;
+    }
+}
+
+public class Solution2 {
+    public int maxProfit(int[] prices) {
+        if(prices == null || prices.length <= 1) {
+            return 0;
+        }
+        int b0 = -prices[0], b1 = b0, s0 = 0, s1 = 0, s2 = 0;
+        for(int i = 1; i < prices.length; i++) {
+            b0 = Math.max(s2 - prices[i], b1);
+            s0 = Math.max(b1 + prices[i], s1);
+            b1 = b0;
+            s2 = s1;
+            s1 = s0;
+        }
+        return s0;
     }
 }
