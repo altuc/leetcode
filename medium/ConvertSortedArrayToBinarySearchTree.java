a306cc697c01e75309e749a1780080edf4a90f9b
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-04 15:09:41 -0400

    Update ConvertSortedArrayToBinarySearchTree.java

--- medium/ConvertSortedArrayToBinarySearchTree.java
+++ medium/ConvertSortedArrayToBinarySearchTree.java
@@ -38,3 +38,23 @@ public class Solution2 {
         return root;
     }
 }
+
+public class Solution3 {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums == null || nums.length == 0) {
+            return null;
+        }
+        return sortedArrayToBSTHelper(nums, 0, nums.length - 1);
+    }
+    
+    public TreeNode sortedArrayToBSTHelper(int[] nums, int start, int end) {
+        if(start > end) {
+            return null;
+        }
+        int mid = (start + end) / 2;
+        TreeNode node = new TreeNode(nums[mid]);
+        node.left = sortedArrayToBSTHelper(nums, start, mid - 1);
+        node.right = sortedArrayToBSTHelper(nums, mid + 1, end);
+        return node;
+    }
+}
