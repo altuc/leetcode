c7a00fa23bf4428d64a461e825491ce54c0b14b8
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-05 14:46:31 -0400

    Update RotateImage.java

--- medium/RotateImage.java
+++ medium/RotateImage.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public void rotate(int[][] matrix) {
         for(int i = 0; i < matrix.length / 2; i++) {
             for(int j = 0; j < matrix[0].length; j++) {
@@ -12,6 +12,26 @@ public class Solution {
                 int temp = matrix[i][j];
                 matrix[i][j] = matrix[j][i];
                 matrix[j][i] = temp;
+            }
+        }
+    }
+}
+
+public class Solution2 {
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n / 2; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[i][n - j - 1];
+                matrix[i][n - j - 1] = temp;
             }
         }
     }
