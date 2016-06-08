64ce3e85f23fde37abfcbd9bd819452d065318bb
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-07 22:25:46 -0400

    Update KthLargestElementInAnArray.java

--- medium/KthLargestElementInAnArray.java
+++ medium/KthLargestElementInAnArray.java
@@ -1,6 +1,19 @@
-public class Solution {
+public class Solution1 {
     public int findKthLargest(int[] nums, int k) {
         Arrays.sort(nums);
         return nums[nums.length - k];
+    }
+}
+
+public class Solution2 {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
+        for(int num : nums) {
+            pq.add(num);
+            if(pq.size() > k) {
+                pq.poll();
+            }
+        }
+        return pq.peek();
     }
 }
