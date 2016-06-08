68001c896f10d6be846f9a932e4deef4889d80dd
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-08 00:41:30 -0400

    Update Subsets.java

--- medium/Subsets.java
+++ medium/Subsets.java
@@ -1,10 +1,11 @@
-public class Solution {
+public class Solution1 {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         List<Integer> list = new ArrayList<Integer>();
         if(nums == null || nums.length == 0) {
             return res;
         }
+        Arrays.sort(nums);
         subsetsHelper(nums, res, list);
         return res;
     }
@@ -18,6 +19,27 @@ public class Solution {
             list.add(nums[i]);
             subsetsHelper(nums, res, list);
             list.remove(list.size() - 1);
+        }
+    }
+}
+
+public class Solution2 {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
+        if(nums == null || nums.length == 0) {
+            return res;
+        }
+        List<Integer> ls = new ArrayList<Integer>();
+        subsetsHelper(nums, res, ls, 0);
+        return res;
+    }
+    
+    public void subsetsHelper(int[] nums, List<List<Integer>> res, List<Integer> ls, int start) {
+        res.add(new ArrayList<Integer>(ls));
+        for(int i = start; i < nums.length; i++) {
+            ls.add(nums[i]);
+            subsetsHelper(nums, res, ls, i + 1);
+            ls.remove(ls.size() - 1);
         }
     }
 }
