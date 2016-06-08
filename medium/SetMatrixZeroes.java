a184a5c5431f8bec057871133e9bce3cf78cdb29
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-07 22:08:54 -0400

    Update SetMatrixZeroes.java

--- medium/SetMatrixZeroes.java
+++ medium/SetMatrixZeroes.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public void setZeroes(int[][] matrix) {
         boolean firstRowZero = false;
         boolean firstColZero = false;
@@ -15,6 +15,44 @@ public class Solution {
         for(int i = 1; i < matrix.length; i++) {
             for(int j = 1; j < matrix[0].length; j++) {
                 if(matrix[i][j] == 0) {
+                    matrix[i][0] = 0;
+                    matrix[0][j] = 0;
+                }
+            }
+        }
+        for(int i = 1; i < matrix.length; i++) {
+            for(int j = 1; j < matrix[0].length; j++) {
+                if(matrix[i][0] == 0 || matrix[0][j] == 0) {
+                    matrix[i][j] = 0;
+                }
+            }
+        }
+        if(firstRowZero) {
+            for(int i = 0; i < matrix[0].length; i++) {
+                matrix[0][i] = 0;
+            }
+        }
+        if(firstColZero) {
+            for(int i = 0; i < matrix.length; i++) {
+                matrix[i][0] = 0;
+            }
+        }
+    }
+}
+
+public class Solution2 {
+    public void setZeroes(int[][] matrix) {
+        boolean firstRowZero = false;
+        boolean firstColZero = false;
+        for(int i = 0; i < matrix.length; i++) {
+            for(int j = 0; j < matrix[0].length; j++) {
+                if(matrix[i][j] == 0) {
+                    if(i == 0) {
+                        firstRowZero = true;
+                    }
+                    if(j == 0) {
+                        firstColZero = true;
+                    }
                     matrix[i][0] = 0;
                     matrix[0][j] = 0;
                 }
