48c3202472131609fc87e9ba5e2602ad4604a01f
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-09 16:57:18 -0400

    Update ReverseInteger.java

--- easy/ReverseInteger.java
+++ easy/ReverseInteger.java
@@ -60,3 +60,21 @@ public class Solution3 {
         return x > 0 ? (int)res : (int)-res; 
     }
 }
+
+public class Solution4 {
+    public int reverse(int x) {
+        int res = 0;
+        while(x != 0) {
+            int temp = x % 10;
+            if((res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && Integer.MAX_VALUE % 10 < temp)) && x > 0) {
+                return 0;
+            }
+            if((res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && Integer.MIN_VALUE % 10 > temp)) && x < 0) {
+                return 0;
+            }
+            res = 10 * res + temp;
+            x /= 10;
+        }
+        return res;
+    }
+}
