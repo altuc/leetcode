dc3d57baba4d4b215eecd7e7453060d278b61fd7
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-08 23:37:51 -0400

    Update ExcelSheetColumnTitle.java

--- easy/ExcelSheetColumnTitle.java
+++ easy/ExcelSheetColumnTitle.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public String convertToTitle(int n) {
         String res = "";
         while(n != 0) {
@@ -7,5 +7,17 @@ public class Solution {
             res = ch + res;
         }
         return res;
+    }
+}
+
+public class Solution2 {
+    public String convertToTitle(int n) {
+        StringBuilder sb = new StringBuilder();
+        while(n != 0) {
+            char c = (char)('A' + (n - 1) % 26);
+            sb.insert(0, c);
+            n = (n - 1) / 26;
+        }
+        return sb.toString();
     }
 }
