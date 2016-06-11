8d77d562b61ef85cf3bb03dca44074fce6e4152b
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-11 16:09:52 -0400

    Update ValidSudoku.java

--- easy/ValidSudoku.java
+++ easy/ValidSudoku.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public boolean isValidSudoku(char[][] board) {
         for(int i = 0; i < 9; i++) {
             ArrayList<Character> list = new ArrayList<Character>();
@@ -29,6 +29,43 @@ public class Solution {
                             return false;
                         } else {
                             list.add(board[m+i][n+j]);
+                        }
+                    }
+                }
+            }
+        }
+        return true;
+    }
+}
+
+public class Solution2 {
+    public boolean isValidSudoku(char[][] board) {
+        if(board == null || board.length == 0 || board[0].length == 0) {
+            return false;
+        }
+        for(int i = 0; i < board.length; i++) {
+            HashSet<Character> set = new HashSet<Character>();
+            for(int j = 0; j < board[0].length; j++) {
+                if(!set.add(board[i][j]) && board[i][j] != '.') {
+                    return false;
+                }
+            }
+        }
+        for(int j = 0; j < board[0].length; j++) {
+            HashSet<Character> set = new HashSet<Character>();
+            for(int i = 0; i < board.length; i++) {
+                if(!set.add(board[i][j]) && board[i][j] != '.') {
+                    return false;
+                }
+            }
+        }
+        for(int i = 0; i < board.length; i += 3) {
+            for(int j = 0; j < board[0].length; j += 3) {
+                HashSet<Character> set = new HashSet<Character>();
+                for(int m = 0; m < 3; m++) {
+                    for(int n = 0; n < 3; n++) {
+                        if(!set.add(board[i + m][j + n]) && board[i + m][j + n] != '.') {
+                            return false;
                         }
                     }
                 }
