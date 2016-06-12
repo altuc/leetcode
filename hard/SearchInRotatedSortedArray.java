3c82adeee63575feeecd5c664d6c42a63bbe8ee9
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-12 17:12:16 -0400

    Update SearchInRotatedSortedArray.java

--- hard/SearchInRotatedSortedArray.java
+++ hard/SearchInRotatedSortedArray.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public int search(int[] nums, int target) {
         for(int i = 0; i < nums.length; i++) {
             if(nums[i] == target) {
@@ -6,5 +6,35 @@ public class Solution {
             }
         }
         return -1;
+    }
+}
+
+public class Solution2 {
+    public int search(int[] nums, int target) {
+        if(nums == null || nums.length == 0) {
+            return -1;
+        }
+        int start = 0;
+        int end = nums.length - 1;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] == target) {
+                return mid;
+            }
+            if(nums[mid] < nums[end]) {
+                if(target > nums[mid] && target <= nums[end]) {
+                    start = mid + 1;
+                } else {
+                    end = mid - 1;
+                }
+            } else {
+                if(target < nums[mid] && target >= nums[start]) {
+                    end = mid - 1;
+                } else {
+                    start = mid + 1;
+                }
+            }
+        }
+        return nums[start] == target ? start : -1;
     }
 }
