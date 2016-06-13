ffc4889c99556f19fb992ec074093ce9966341f8
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-12 23:12:26 -0400

    Update CombinationSum.java

--- medium/CombinationSum.java
+++ medium/CombinationSum.java
@@ -2,6 +2,9 @@ public class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         List<Integer> list = new ArrayList<Integer>();
+        if(candidates == null || candidates.length == 0) {
+            return res;
+        }
         Arrays.sort(candidates);
         combinationSumHelper(candidates, target, res, list, 0);
         return res;
@@ -12,12 +15,12 @@ public class Solution {
             res.add(new ArrayList<Integer>(list));
             return;
         }
-        if(target < 0) {
-            return;
-        }
         for(int i = start; i < candidates.length; i++) {
             if(i > start && candidates[i] == candidates[i - 1]) {
                 continue;
+            }
+            if(target - candidates[i] < 0) {
+                return;
             }
             list.add(candidates[i]);
             combinationSumHelper(candidates, target - candidates[i], res, list, i);
