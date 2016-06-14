bc101b89efc4ad327520a80b868f79a15690272e
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-14 15:09:41 -0400

    Update H-Index.java

--- medium/H-Index.java
+++ medium/H-Index.java
@@ -1,4 +1,4 @@
-public class Solution {
+public class Solution1 {
     public int hIndex(int[] citations) {
         if(citations == null || citations.length == 0) {
             return 0;
@@ -13,3 +13,19 @@ public class Solution {
         return h;
     }
 }
+
+public class Solution2 {
+    public int hIndex(int[] citations) {
+        if(citations == null || citations.length == 0) {
+            return 0;
+        }
+        Arrays.sort(citations);
+        for(int i = 0; i < citations.length; i++) {
+            if(citations[i] >= citations.length - i) {
+                return citations.length - i;
+            }
+        }
+        return 0;
+    }
+}
+
