47e7f2be1199d420fb8e857cb822f452c44f38c2
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-16 14:04:43 -0400

    committing verifypreorderserializationofbinarytree

---
+++ medium/VerifyPreorderSerializationOfABinaryTree.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public boolean isValidSerialization(String preorder) {
+        if(preorder == null || preorder.isEmpty()) {
+            return false;
+        }
+        String[] nodes = preorder.split(",");
+        if(nodes.length % 2 == 0) {
+            return false;
+        }
+        if(!("#").equals(nodes[nodes.length - 1])) {
+            return false;
+        }
+        int isNull = 0;
+        int isNotNull = 0;
+        for(int i = 0; i < nodes.length - 1; i++) {
+            if(("#").equals(nodes[i])) {
+                isNull++;
+            } else {
+                isNotNull++;
+            }
+            if(isNull > isNotNull) {
+                return false;
+            }
+        }
+        isNull += 1;
+        if(isNotNull != isNull - 1) {
+            return false;
+        }
+        return true;
+    }
+}
