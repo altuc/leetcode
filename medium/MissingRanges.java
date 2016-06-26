84a02d54ae0c254a8c32a2e59db16769b020ef55
beeacoder <zhou.hanj@husky.neu.edu>
2016-06-25 23:03:40 -0400

    committing leetcode missingranges

---
+++ medium/MissingRanges.java
@@ -0,0 +1,49 @@
+public class Solution {
+    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
+        List<String> res = new ArrayList<String>();
+        String s = "";
+        if(nums == null || nums.length == 0) {
+            if(lower == upper) {
+                s = Integer.toString(lower);
+            } else {
+                s = Integer.toString(lower) + "->" + Integer.toString(upper);
+            }
+            res.add(s);
+            return res;
+        }
+        if(nums[0] > lower) {
+            s = Integer.toString(lower);
+        }
+        for(int i = 0; i < nums.length; i++) {
+            if(i == 0) {
+                if(nums[i] > lower && nums[i] == lower + 1) {
+                    res.add(s);
+                } 
+                if(nums[i] > lower && nums[i] != lower + 1) {
+                    s = s + "->" + Integer.toString(nums[i] - 1);
+                    res.add(s);
+                }
+                continue;
+            }
+            if(nums[i] != nums[i - 1] + 1) {
+                s = Integer.toString(nums[i - 1] + 1);
+                if(nums[i] == nums[i - 1] + 2) {
+                    res.add(s);
+                } else {
+                    s = s + "->" + Integer.toString(nums[i] - 1);
+                    res.add(s);
+                }
+            }
+        }
+        if(nums[nums.length - 1] < upper) {
+            s = Integer.toString(nums[nums.length - 1] + 1);
+            if(upper == nums[nums.length - 1] + 1) {
+                res.add(s);
+            } else {
+                s = s + "->" + Integer.toString(upper);
+                res.add(s);
+            }
+        }
+        return res;
+    }
+}
