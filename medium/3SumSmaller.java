37abdd46aa412213555b862a92eb24c86145dca2
beeacoder <zhou.hanj@husky.neu.edu>
2016-07-02 15:26:13 -0400

    committing leetcode 3sumsmaller

---
+++ medium/3SumSmaller.java
@@ -0,0 +1,22 @@
+public class Solution {
+    public int threeSumSmaller(int[] nums, int target) {
+        if(nums == null || nums.length == 0) {
+            return 0;
+        }
+        Arrays.sort(nums);
+        int count = 0;
+        for(int i = 0; i < nums.length - 2; i++) {
+            int j = i + 1;
+            int k = nums.length - 1;
+            while(j < k) {
+                if(nums[i] + nums[j] + nums[k] < target) {
+                    count += k - j;
+                    j++;
+                } else {
+                    k--;
+                }
+            }
+        }
+        return count;
+    }
+}
