574402099c1135fffc82fefd1a57a32a97937008
beeacoder <zhou.hanj@husky.neu.edu>
2016-07-03 13:41:23 -0400

    committing leetcode movingaveragefromdatastream

---
+++ easy/MovingAverageFromDataStream.java
@@ -0,0 +1,33 @@
+public class MovingAverage {
+    
+    Queue<Integer> q = null;
+    int max;
+    double sum = 0.0;
+
+    /** Initialize your data structure here. */
+    public MovingAverage(int size) {
+        if(size <= 0) {
+            return;
+        }
+        q = new LinkedList<Integer>();
+        max = size;
+    }
+    
+    public double next(int val) {
+        if(q.size() < max) {
+            q.add(val);
+            sum += val;
+        } else {
+            sum -= q.poll();
+            q.add(val);
+            sum += val;
+        }
+        return sum / q.size();
+    }
+}
+
+/**
+ * Your MovingAverage object will be instantiated and called as such:
+ * MovingAverage obj = new MovingAverage(size);
+ * double param_1 = obj.next(val);
+ */
