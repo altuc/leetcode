b43f66b171459a5a9de3650508da0b7638f82999
beeacoder <zhou.hanj@husky.neu.edu>
2016-07-06 22:25:23 -0400

    committing leetcode lrucache

---
+++ hard/LRUCache.java
@@ -0,0 +1,70 @@
+public class LRUCache {
+    
+    int cap;
+    HashMap<Integer, Node> map = null;
+    Node head, tail;
+    
+    public LRUCache(int capacity) {
+        cap = capacity;
+        map = new HashMap<Integer, Node>();
+        head = new Node(0, 0);
+        tail = new Node(0, 0);
+        head.next = tail;
+        head.pre = null;
+        tail.next = null;
+        tail.pre = head;
+    }
+    
+    public int get(int key) {
+        if(map.containsKey(key)) {
+            Node n = map.get(key);
+            removeNode(n);
+            moveNodeToHead(n);
+            return n.value;
+        } else {
+            return -1;
+        }
+    }
+    
+    public void set(int key, int value) {
+        if(map.containsKey(key)) {
+            Node n = map.get(key);
+            n.value = value;
+            removeNode(n);
+            moveNodeToHead(n);
+        } else {
+            if(cap == map.size()) {
+                map.remove(tail.pre.key);
+                removeNode(tail.pre);
+            } 
+            Node n = new Node(key, value);
+            map.put(key, n);
+            moveNodeToHead(n);
+        }
+    }
+    
+    public void moveNodeToHead(Node node) {
+        node.next = head.next;
+        node.next.pre = node;
+        node.pre = head;
+        head.next = node;
+    }
+    
+    public void removeNode(Node node) {
+        node.pre.next = node.next;
+        node.next.pre = node.pre;
+    }
+}
+
+class Node {
+    
+    int key;
+    int value;
+    Node pre;
+    Node next;
+    
+    public Node(int key, int value) {
+        this.key = key;
+        this.value = value;
+    }
+}
