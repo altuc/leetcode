e4ea8053c03471fc0d8c18c4489854385db3347a
beeacoder <zhou.hanj@husky.neu.edu>
2016-07-22 23:09:33 -0400

    Update GeneralizedAbbreviation.java

--- medium/GeneralizedAbbreviation.java
+++ medium/GeneralizedAbbreviation.java
@@ -8,13 +8,13 @@ public class Solution {
         return res;
     }
     
-    public void generateAbbreviationsHelper(String word, List<String> res, String cur, int pos, int count) {
-        if(pos == word.length()) {
+    public void generateAbbreviationsHelper(String word, List<String> res, String cur, int start, int count) {
+        if(start == word.length()) {
             cur = count > 0 ? cur + count : cur;
             res.add(cur);
             return;
         }
-        generateAbbreviationsHelper(word, res, cur, pos + 1, count + 1);
-        generateAbbreviationsHelper(word, res, cur + (count > 0 ? count : "") + word.charAt(pos), pos + 1, 0);
+        generateAbbreviationsHelper(word, res, cur, start + 1, count + 1);
+        generateAbbreviationsHelper(word, res, cur + (count > 0 ? count : "") + word.charAt(start), start + 1, 0);
     }
 }
