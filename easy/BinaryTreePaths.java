7857ac323ace49137e9d5bc8740229398273d5e3
beeacoder <zhou.hanj@husky.neu.edu>
2016-07-23 13:48:57 -0400

    Update BinaryTreePaths.java

--- easy/BinaryTreePaths.java
+++ easy/BinaryTreePaths.java
@@ -7,7 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution {
+public class Solution1 {
     public List<String> binaryTreePaths(TreeNode root) {
         ArrayList<String> res = new ArrayList<>();
         if(root == null) {
@@ -32,6 +32,32 @@ public class Solution {
             binaryTreePathsHelper(right.left, right.right, path1, res);
             String path2 = path + "->" + Integer.toString(left.val);
             binaryTreePathsHelper(left.left, left.right, path2, res);
+        }
+    }
+}
+
+public class Solution2 {
+    public List<String> binaryTreePaths(TreeNode root) {
+        List<String> res = new ArrayList<String>();
+        if(root == null) {
+            return res;
+        }
+        binaryTreePathsHelper(root, res, "");
+        return res;
+    }
+    
+    public void binaryTreePathsHelper(TreeNode node, List<String> res, String path) {
+        if(node.left == null && node.right == null) {
+            path += node.val;
+            res.add(path);
+            return;
+        } else if(node.left != null && node.right == null) {
+            binaryTreePathsHelper(node.left, res, path + node.val + "->");
+        } else if(node.left == null && node.right != null) {
+            binaryTreePathsHelper(node.right, res, path + node.val + "->");
+        } else {
+            binaryTreePathsHelper(node.left, res, path + node.val + "->");
+            binaryTreePathsHelper(node.right, res, path + node.val + "->");
         }
     }
 }
