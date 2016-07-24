5353b7f2c8c81d700a9c0c73af286da94ec5bcd5
beeacoder <zhou.hanj@husky.neu.edu>
2016-07-24 00:43:49 -0400

    Update Flatten2DVector.java

--- medium/Flatten2DVector.java
+++ medium/Flatten2DVector.java
@@ -1,4 +1,4 @@
-public class Vector2D implements Iterator<Integer> {
+public class Vector2D1 implements Iterator<Integer> {
     
     private Queue<Integer> q = null;
 
@@ -17,6 +17,37 @@ public class Vector2D implements Iterator<Integer> {
     @Override
     public boolean hasNext() {
         return !q.isEmpty();
+    }
+}
+
+public class Vector2D2 implements Iterator<Integer> {
+    
+    private Iterator<List<Integer>> ite1 = null;
+    private Iterator<Integer> ite2 = null;
+
+    public Vector2D(List<List<Integer>> vec2d) {
+        if(vec2d == null || vec2d.isEmpty()) {
+            return;
+        }
+        ite1 = vec2d.iterator();
+        ite2 = ite1.next().iterator();
+        while(!ite2.hasNext() && ite1.hasNext()) {
+            ite2 = ite1.next().iterator();
+        }
+    }
+
+    @Override
+    public Integer next() {
+        int next = ite2.next();
+        while(!ite2.hasNext() && ite1.hasNext()) {
+            ite2 = ite1.next().iterator();
+        }
+        return next;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return ite2 != null && ite2.hasNext();
     }
 }
 
