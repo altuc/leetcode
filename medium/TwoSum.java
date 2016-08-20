199dbcdf8e8f872a355e245a160e6affb563794b
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-20 13:11:28 -0400

    Update TwoSum.java

--- medium/TwoSum.java
+++ medium/TwoSum.java
@@ -1,16 +1,16 @@
 public class Solution {
     public int[] twoSum(int[] nums, int target) {
+        int[] res = new int[2];
         HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-        int[] indices = new int[2];
         for(int i = 0; i < nums.length; i++) {
             if(map.containsKey(target - nums[i])) {
-                indices[0] = map.get(target - nums[i]) + 1;
-                indices[1] = i + 1;
+                res[0] = map.get(target - nums[i]);
+                res[1] = i;
                 break;
             } else {
                 map.put(nums[i], i);
             }
         }
-        return indices;
+        return res;
     }
 }
