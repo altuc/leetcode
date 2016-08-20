0b2865fe8f217debf05c2ed49ad9aaf27587c91a
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-20 19:00:02 -0400

    Update IsomorphicStrings.java

--- easy/IsomorphicStrings.java
+++ easy/IsomorphicStrings.java
@@ -1,32 +1,6 @@
-public class Solution1 {
+public class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if(s.isEmpty() && t.isEmpty()) {
-            return true;
-        }
-        return isIsomorphicHelper(s, t) && isIsomorphicHelper(t, s);
-    }
-    
-    public boolean isIsomorphicHelper(String s, String t) {
-        HashMap<Character, Character> map = new HashMap<>();
-        for(int i = 0; i < s.length(); i++) {
-            if(!map.containsKey(s.charAt(i))) {
-                map.put(s.charAt(i), t.charAt(i));
-            } else {
-                if(t.charAt(i) != map.get(s.charAt(i))) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-}
-
-public class Solution2 {
-    public boolean isIsomorphic(String s, String t) {
-        if(s.isEmpty() && t.isEmpty()) {
-            return true;
-        }
-        HashMap<Character, Character> map = new HashMap<>();
+        HashMap<Character, Character> map = new HashMap<Character, Character>();
         for(int i = 0; i < s.length(); i++) {
             if(!map.containsKey(s.charAt(i))) {
                 if(map.containsValue(t.charAt(i))) {
