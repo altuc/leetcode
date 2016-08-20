7f1061ecae0a9da683996b2df7b709915df0497e
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-20 18:25:12 -0400

    Update TwoSum-III-DataStructureDesign.java

--- easy/TwoSum-III-DataStructureDesign.java
+++ easy/TwoSum-III-DataStructureDesign.java
@@ -3,7 +3,7 @@ public class TwoSum {
     private HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
     
     // Add the number to an internal data structure.
-	  public void add(int number) {
+    public void add(int number) {
         if(map.containsKey(number)) {
             map.put(number, 2);
         } else {
@@ -12,13 +12,13 @@ public class TwoSum {
     }
 
     // Find if there exists any pair of numbers which sum is equal to the value.
-	  public boolean find(int value) {
-	      for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
-	          int number1 = entry.getKey();
-	          int number2 = value - number1;
-	          if((number1 == number2 && entry.getValue() == 2) || (number1 != number2 && map.containsKey(number2))) {
-	              return true;
-	          }
+    public boolean find(int value) {
+        for(Map.Entry<Integer, Integer> entry : map.entrySet()) {
+	    int number1 = entry.getKey();
+	    int number2 = value - number1;
+	    if((number1 == number2 && entry.getValue() == 2) || (number1 != number2 && map.containsKey(number2))) {
+	        return true;
+	    }
         }
         return false;
     }
