822a8f00d7aa9f7a7a2b5f4d1bb267f357e77f3d
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-29 11:29:11 -0400

    Update MergeSortedArray.java

--- easy/MergeSortedArray.java
+++ easy/MergeSortedArray.java
@@ -1,17 +1,17 @@
 public class Solution {
-    public void merge(int A[], int m, int B[], int n) {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
         while(m > 0 && n > 0) {
-            if(A[m-1] <= B[n-1]) {
-                A[m+n-1] = B[n-1];
-                n -= 1;
+            if(nums1[m - 1] > nums2[n - 1]) {
+                nums1[m + n - 1] = nums1[m - 1];
+                m--;
             } else {
-                A[m+n-1] = A[m-1];
-                m -= 1;
+                nums1[m + n - 1] = nums2[n - 1];
+                n--;
             }
         }
         while(n > 0) {
-            A[m+n-1] = B[n-1];
-            n -= 1;
+            nums1[n - 1] = nums2[n - 1];
+            n--;
         }
     }
 }
