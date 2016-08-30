b62054361f43210059ecc2b358273ab8ddc8cd69
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-29 22:46:31 -0400

    Update RemoveDuplicatesFromSortedArray.java

--- easy/RemoveDuplicatesFromSortedArray.java
+++ easy/RemoveDuplicatesFromSortedArray.java
@@ -1,15 +1,15 @@
 public class Solution {
     public int removeDuplicates(int[] nums) {
-        if(nums.length == 0 || nums.length == 1) {
-            return nums.length;
+        if(nums == null || nums.length == 0) {
+            return 0;
         }
-        int len = 1;
+        int left = 1;
         for(int i = 1; i < nums.length; i++) {
             if(nums[i] != nums[i - 1]) {
-                nums[len] = nums[i];
-                len++;
+                nums[left] = nums[i];
+                left++;
             }
         }
-        return len;
+        return left;
     }
 }
