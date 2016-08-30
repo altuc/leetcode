7441fcf74e7cdc563c9a4b217102c0251f64bcec
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-30 10:44:16 -0400

    Update PascalsTriangle.java

--- easy/PascalsTriangle.java
+++ easy/PascalsTriangle.java
@@ -1,39 +1,35 @@
 public class Solution1 {
     public List<List<Integer>> generate(int numRows) {
-        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
+        List<List<Integer>> res = new ArrayList<List<Integer>>();
         if(numRows <= 0) {
             return res;
-        } else {
-            ArrayList<Integer> toplevel = new ArrayList<Integer>();
-            toplevel.add(1);
-            res.add(toplevel);
-            if(numRows > 1) {
-                int n = 1;
-                ArrayList<Integer> uplevel = toplevel;
-                while(n < numRows) {
-                    ArrayList<Integer> level = new ArrayList<Integer>();
-                    level.add(1);
-                    for(int i = 1; i < n; i++) {
-                        level.add(uplevel.get(i-1) + uplevel.get(i));
-                    }
-                    level.add(1);
-                    res.add(level);
-                    uplevel = level;
-                    n++;
-                }
-            }
-            return res;
         }
+        List<Integer> pre = new ArrayList<Integer>();
+        pre.add(1);
+        res.add(pre);
+        int level = 1;
+        while(level < numRows) {
+            List<Integer> row = new ArrayList<Integer>();
+            row.add(1);
+            for(int i = 1; i < level; i++) {
+                row.add(pre.get(i - 1) + pre.get(i));
+            }
+            row.add(1);
+            res.add(row);
+            pre = row;
+            level++;
+        }
+        return res;
     }
 }
 
 public class Solution2 {
     public List<List<Integer>> generate(int numRows) {
-        ArrayList<List<Integer>> rs = new ArrayList<List<Integer>>();
-        if(numRows == 0) {
+        List<List<Integer>> rs = new ArrayList<List<Integer>>();
+        if(numRows <= 0) {
             return rs;
         }
-        ArrayList<Integer> ls = new ArrayList<Integer>();
+        List<Integer> ls = new ArrayList<Integer>();
         ls.add(1);
         rs.add(ls);
         return generateHelper(numRows, rs, ls);
@@ -43,11 +39,11 @@ public class Solution2 {
         if(numRows == 1) {
             return rs;
         }
-        ArrayList<Integer> row = new ArrayList<Integer>();
+        List<Integer> row = new ArrayList<Integer>();
         row.add(1);
         if(ls.size() > 1) {
-            for(int i = 0; i < ls.size()-1; i++) {
-                row.add(ls.get(i) + ls.get(i+1));
+            for(int i = 0; i < ls.size() - 1; i++) {
+                row.add(ls.get(i) + ls.get(i + 1));
             }
         }
         row.add(1);
