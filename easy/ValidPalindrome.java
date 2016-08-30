92dd384ecbe4dfa89ceb62a46533e808bc355fa2
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-30 15:56:42 -0400

    Update ValidPalindrome.java

--- easy/ValidPalindrome.java
+++ easy/ValidPalindrome.java
@@ -1,75 +1,22 @@
-public class Solution1 {
+public class Solution {
     public boolean isPalindrome(String s) {
-        if(s.isEmpty()) {
+        if(s == null || s.trim().isEmpty()) {
             return true;
         }
-        String ss = s.toLowerCase();
-        Stack<Character> sta = new Stack<Character>();
-        for(char c : ss.toCharArray()) {
-            if((c <= 'z' && c >= 'a') || (c <= '9' && c >= '0')) {
-                sta.push(c);
-            }
-        }
-        for(char c : ss.toCharArray()) {
-            if((c <= 'z' && c >= 'a') || (c <= '9' && c >= '0')) {
-                if(c != sta.pop()) {
-                    return false;
-                }
-            }
-        }
-        return true;
-    }
-}
-
-public class Solution2 {
-    public boolean isPalindrome(String s) {
-        if(s == null || s.isEmpty()) {
-            return true;
-        }
-        String str = s.toLowerCase();
-        int start = 0;
-        int end = s.length() - 1;
-        while(start < end) {
-            char left = str.charAt(start);
-            char right = str.charAt(end);
-            if(((left >= 'a' && left <= 'z') || (left >= '0' && left <= '9')) && ((right >= 'a' && right <= 'z') || (right >= '0' && right <= '9'))) {
-                if(left != right) {
-                    return false;
-                }
-                start++;
-                end--;
-            } else if(!((left >= 'a' && left <= 'z') || (left >= '0' && left <= '9')) && ((right >= 'a' && right <= 'z') || (right >= '0' && right <= '9'))) {
-                start++;
-            } else if(((left >= 'a' && left <= 'z') || (left >= '0' && left <= '9')) && !((right >= 'a' && right <= 'z') || (right >= '0' && right <= '9'))) {
-                end--;
+        s = s.trim().toLowerCase();
+        int left = 0;
+        int right = s.length() - 1;
+        while(left < right) {
+            if(s.charAt(left) < '0' || (s.charAt(left) > '9' && s.charAt(left) < 'a') || s.charAt(left) > 'z') {
+                left++;
+            } else if(s.charAt(right) < '0' || (s.charAt(right) > '9' && s.charAt(right) < 'a') || s.charAt(right) > 'z') {
+                right--;
             } else {
-                start++;
-                end--;
-            }
-        }
-        return true;
-    }
-}
-
-public class Solution3 {
-    public boolean isPalindrome(String s) {
-        if(s == null || s.isEmpty()) {
-            return true;
-        }
-        int start = 0;
-        int end = s.length() - 1;
-        String str = s.toLowerCase();
-        while(start < end) {
-            if(str.charAt(start) < '0' || (str.charAt(start) > '9' && str.charAt(start) < 'a') || str.charAt(start) > 'z') {
-                start++;
-            } else if(str.charAt(end) < '0' || (str.charAt(end) > '9' && str.charAt(end) < 'a') || str.charAt(end) > 'z') {
-                end--;
-            } else {
-                if(str.charAt(start) != str.charAt(end)) {
+                if(s.charAt(left) != s.charAt(right)) {
                     return false;
                 }
-                start++;
-                end--;
+                left++;
+                right--;
             }
         }
         return true;
