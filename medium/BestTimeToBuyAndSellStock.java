73b3ea1cd8ba5529219978420b4937640d030698
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-30 15:02:28 -0400

    Update BestTimeToBuyAndSellStock.java

--- medium/BestTimeToBuyAndSellStock.java
+++ medium/BestTimeToBuyAndSellStock.java
@@ -1,16 +1,14 @@
 public class Solution {
     public int maxProfit(int[] prices) {
-        if(prices.length <= 1) {
+        if(prices == null || prices.length <= 1) {
             return 0;
         }
-        int profit = 0;
         int min = prices[0];
-        for(int i = 0; i < prices.length; i++) {
-            if(min > prices[i]) {
-                min = prices[i];
-            }
-            profit = Math.max(profit, prices[i] - min);
+        int res = 0;
+        for(int i = 1; i < prices.length; i++) {
+            res = Math.max(res, prices[i] - min);
+            min = Math.min(min, prices[i]);
         }
-        return profit;
+        return res;
     }
 }
