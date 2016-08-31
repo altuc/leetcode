2d44c4f6e8eabde2bc56e4067cc2733b9ea24e59
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-30 23:19:14 -0400

    Update RotateArray.java

--- easy/RotateArray.java
+++ easy/RotateArray.java
@@ -1,5 +1,8 @@
 public class Solution {
     public void rotate(int[] nums, int k) {
+        if(nums == null || nums.length <= 1) {
+            return;
+        }
         k %= nums.length;
         reverse(nums, 0, nums.length - 1);
         reverse(nums, 0, k - 1);
