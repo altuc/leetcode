e7d2d696bfc3804b145de4d950f99c7d2358ba49
beeacoder <zhou.hanj@husky.neu.edu>
2016-08-31 22:00:11 -0400

    Update FlipGame.java

--- easy/FlipGame.java
+++ easy/FlipGame.java
@@ -23,7 +23,7 @@ public class Solution2 {
         List<String> res = new ArrayList<String>();
         for(int i = 0; i < s.length() - 1; i++) {
             if(s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
-                res.add(s.substring(0, i) + "--" + s.substring(i + 2, s.length()));
+                res.add(s.substring(0, i) + "--" + s.substring(i + 2));
             }
         }
         return res;
