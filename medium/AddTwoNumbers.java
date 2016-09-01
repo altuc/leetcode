f08ec703b7ad47bb4c18a8ab127123a88605f931
beeacoder <zhou.hanj@husky.neu.edu>
2016-09-01 17:31:28 -0400

    Update AddTwoNumbers.java

--- medium/AddTwoNumbers.java
+++ medium/AddTwoNumbers.java
@@ -17,11 +17,11 @@ public class Solution1 {
         if(l2 == null) {
             return l1;
         }
-        int sum = 0;
         int carry = 0;
         ListNode sentinel = new ListNode(0);
-        ListNode current = sentinel;
+        ListNode cur = sentinel;
         while(l1 != null || l2 != null) {
+            int sum = carry;
             if(l1 != null) {
                 sum += l1.val;
                 l1 = l1.next;
@@ -30,18 +30,16 @@ public class Solution1 {
                 sum += l2.val;
                 l2 = l2.next;
             }
-            sum += carry;
-            current.next = new ListNode(sum % 10);
-            current = current.next;
-            if(sum > 9) {
+            if(sum >= 10) {
                 carry = 1;
             } else {
                 carry = 0;
             }
-            sum = 0;
+            cur.next = new ListNode(sum % 10);
+            cur = cur.next;
         }
         if(carry == 1) {
-            current.next = new ListNode(1);
+            cur.next = new ListNode(1);
         }
         return sentinel.next;
     }
@@ -75,43 +73,5 @@ public class Solution2 {
            }
        }
        return sentinel.next;
-    }
-}
-
-public class Solution3 {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        if(l1 == null) {
-            return l2;
-        }
-        if(l2 == null) {
-            return l1;
-        }
-        ListNode sentinel = new ListNode(0);
-        ListNode cur = sentinel;
-        int carry = 0;
-        while(l1 != null || l2 != null) {
-            if(l1 != null && l2 != null) {
-                int val = l1.val + l2.val + carry;
-                carry = val >= 10 ? 1 : 0;
-                cur.next = new ListNode(val % 10);
-                l1 = l1.next;
-                l2 = l2.next;
-            } else if(l1 != null) {
-                int val = l1.val + carry;
-                carry = val >= 10 ? 1 : 0;
-                cur.next = new ListNode(val % 10);
-                l1 = l1.next;
-            } else {
-                int val = l2.val + carry;
-                carry = val >= 10 ? 1 : 0;
-                cur.next = new ListNode(val % 10);
-                l2 = l2.next;
-            }
-            cur = cur.next;
-        }
-        if(carry == 1) {
-            cur.next = new ListNode(1);
-        }
-        return sentinel.next;
     }
 }
