1e2a036b171ee6d93a01bc27f8683721070bb98b
beeacoder <zhou.hanj@husky.neu.edu>
2016-09-01 21:37:13 -0400

    Update LowestCommonAncestorOfABinarySearchTree.java

--- easy/LowestCommonAncestorOfABinarySearchTree.java
+++ easy/LowestCommonAncestorOfABinarySearchTree.java
@@ -54,3 +54,18 @@ public class Solution3 {
         return root;
     }
 }
+
+public class Solution4 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        while(root != null) {
+            if(root.val > p.val && root.val > q.val) {
+                root = root.left;
+            } else if(root.val < p.val && root.val < q.val) {
+                root = root.right;
+            } else {
+                return root;
+            }
+        }
+        return root;
+    }
+}
