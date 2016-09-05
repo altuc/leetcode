79c61eed37d51459123c3817da412b2726e874da
beeacoder <zhou.hanj@husky.neu.edu>
2016-09-05 16:43:19 -0400

    Update JumpGame.java

--- medium/JumpGame.java
+++ medium/JumpGame.java
@@ -1,11 +1,32 @@
-public class Solution {
+public class Solution1 {
     public boolean canJump(int[] nums) {
+        if(nums == null || nums.length <= 1) {
+            return true;
+        }
         int max = 0;
         for(int i = 0; i < nums.length; i++) {
             if(i > max) {
                 return false;
             }
             max = Math.max(nums[i] + i, max);
+        }
+        return true;
+    }
+}
+
+public class Solution2 {
+    public boolean canJump(int[] nums) {
+        if(nums == null || nums.length <= 1) {
+            return true;
+        }
+        int i = 0;
+        int max = 0;
+        while(max < nums.length - 1) {
+            if(nums[max] == 0 && i == max) {
+                return false;
+            }
+            max = Math.max(max, i + nums[i]);
+            i++;
         }
         return true;
     }
