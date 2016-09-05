cde7348f526f1028aa0a45cc62f0744d858a4725
beeacoder <zhou.hanj@husky.neu.edu>
2016-09-05 18:04:48 -0400

    Update PermutationSequence.java

--- medium/PermutationSequence.java
+++ medium/PermutationSequence.java
@@ -1,6 +1,6 @@
 public class Solution {
     public String getPermutation(int n, int k) {
-        ArrayList<Integer> list = new ArrayList<Integer>();
+        List<Integer> list = new ArrayList<Integer>();
         int factorial = 1;
         for(int i = 1; i <= n; i++) {
             list.add(i);
@@ -8,7 +8,7 @@ public class Solution {
         }
         k--;
         StringBuilder sb = new StringBuilder();
-        for(int i = n; i > 0; i--) {
+        for(int i = n; i >= 1; i--) {
             factorial /= i;
             sb.append(list.remove(k / factorial));
             k %= factorial;
