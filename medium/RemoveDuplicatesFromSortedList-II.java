ce7ef04d2831d69d46d0a4458c41b9256e2e7766
beeacoder <zhou.hanj@husky.neu.edu>
2016-09-06 17:32:14 -0400

    Update RemoveDuplicatesFromSortedList-II.java

--- medium/RemoveDuplicatesFromSortedList-II.java
+++ medium/RemoveDuplicatesFromSortedList-II.java
@@ -11,20 +11,16 @@ public class Solution {
         if(head == null || head.next == null) {
             return head;
         }
-        ListNode sentinel = new ListNode(0);
-        sentinel.next = head;
-        ListNode p1 = sentinel;
-        while(p1.next != null) {
-            ListNode p2 = p1.next;
-            while(p2.next != null && p2.val == p2.next.val) {
-                p2 = p2.next;
-            }
-            if(p2 == p1.next) {
-                p1 = p1.next;
-            } else {
-                p1.next = p2.next;
-            }
+        ListNode cur = head;
+        ListNode remain = head;
+        while(cur.next != null) {
+            if(cur.next.val != cur.val) {
+                remain.next = cur.next;
+                remain = remain.next;
+            } 
+            cur = cur.next;
         }
-        return sentinel.next;
+        remain.next = null;
+        return head;
     }
 }
