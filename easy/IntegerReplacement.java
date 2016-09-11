9ec95824c50e2bffe9d00923af51a399ec7c1ec2
beeacoder <zhou.hanj@husky.neu.edu>
2016-09-11 16:34:05 -0400

    committing leetcode integerreplacement

---
+++ easy/IntegerReplacement.java
@@ -0,0 +1,16 @@
+public class Solution {
+    public int integerReplacement(int n) {
+        long ln = (long) n;
+        return integerReplacementHelper(ln, 0);
+    }
+    
+    public int integerReplacementHelper(long n, int count) {
+        if(n == 1) {
+            return count;
+        } else if(n % 2 == 0) {
+            return integerReplacementHelper(n / 2, count + 1);
+        } else {
+            return Math.min(integerReplacementHelper(n + 1, count + 1), integerReplacementHelper(n - 1, count + 1));
+        }
+    }
+}
