a1d6252bc268958ad5285e3c041a032b74d29d39
beeacoder <zhou.hanj@husky.neu.edu>
2016-10-28 11:07:03 -0400

    Update ZigZagConversion.java

--- easy/ZigZagConversion.java
+++ easy/ZigZagConversion.java
@@ -1,14 +1,15 @@
 public class Solution {
-    public String convert(String s, int nRows) {
-        if(s == null || nRows <= 1 || s.length() <= nRows) {
+    public String convert(String s, int numRows) {
+        if(s == null || s.length() <= numRows || numRows == 1) {
             return s;
         }
+        int len = s.length();
+        int unit = 2 * numRows - 2;
         StringBuilder sb = new StringBuilder();
-        int unit = 2 * nRows - 2;
-        for(int i = 0; i < nRows; i++) {
-            for(int j = i; j < s.length(); j += unit) {
+        for(int i = 0; i < numRows; i++) {
+            for(int j = i; j < len; j += unit) {
                 sb.append(s.charAt(j));
-                if(i != 0 && i != nRows - 1 && j + unit - 2 * i < s.length()) {
+                if(i != 0 && i != numRows - 1 && j + unit - 2 * i < len) {
                     sb.append(s.charAt(j + unit - 2 * i));
                 }
             }
