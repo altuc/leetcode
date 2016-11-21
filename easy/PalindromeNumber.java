5a029e8d6be1287014f8633a04808ae0a3bc2c27
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-21 15:35:51 -0500

    Update PalindromeNumber.java

--- easy/PalindromeNumber.java
+++ easy/PalindromeNumber.java
@@ -1,16 +1,16 @@
 public class Solution {
     public boolean isPalindrome(int x) {
-        if (x < 0) {
+        if(x < 0) {
             return false;
         }
         int div = 1;
-        while (x / div >= 10) {
+        while(x / div >= 10) {
             div *= 10;
         }
-        while (x != 0) {
+        while(x != 0) {
             int left = x / div; 
             int right = x % 10;
-            if (left != right) {
+            if(left != right) {
                 return false;
             }
             x = (x % div) / 10;
