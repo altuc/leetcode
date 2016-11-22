13255772bb6223d97a65455947e57ed31b4d2f83
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-22 15:29:53 -0500

    Update PlusOne.java

--- easy/PlusOne.java
+++ easy/PlusOne.java
@@ -1,10 +1,10 @@
 public class Solution {
     public int[] plusOne(int[] digits) {
         for(int i = digits.length - 1; i >= 0; i--) {
-            digits[i] += 1;
-            if(digits[i] == 10) {
+            if(digits[i] == 9) {
                 digits[i] = 0;
             } else {
+                digits[i] += 1;
                 return digits;
             }
         }
