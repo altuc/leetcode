f7e9f16fc35e2c9c6351a4f7a300747c2d194054
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-22 13:33:36 -0500

    Update RemoveElement.java

--- easy/RemoveElement.java
+++ easy/RemoveElement.java
@@ -3,13 +3,13 @@ public class Solution {
         if(nums == null || nums.length == 0) {
             return 0;
         }
-        int left = 0;
-        for(int i = 0; i < nums.length; i++) {
+        int pos = 0;
+        int len = nums.length;
+        for(int i = 0; i < len; i++) {
             if(nums[i] != val) {
-                nums[left] = nums[i];
-                left++;
+                nums[pos++] = nums[i];
             }
         }
-        return left;
+        return pos;
     }
 }
