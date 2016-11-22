c397a8a5fb746393c8bb578899898f778363bfc8
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-22 15:56:40 -0500

    Update AddBinary.java

--- easy/AddBinary.java
+++ easy/AddBinary.java
@@ -1,27 +1,21 @@
 public class Solution {
     public String addBinary(String a, String b) {
-        if(a == null || a.isEmpty()) {
-            return b;
-        }
-        if(b == null || b.isEmpty()) {
-            return a;
-        }
-        int pa = a.length() - 1;
-        int pb = b.length() - 1;
-        int carry = 0;
         StringBuilder sb = new StringBuilder();
-        while(pa >= 0 || pb >= 0) {
-            int sum = carry;
-            if(pa >= 0) {
-                sum += a.charAt(pa) - '0';
-                pa--;
+        int posA = a.length() - 1;
+        int posB = b.length() - 1;
+        int carry = 0;
+        while(posA >= 0 || posB >= 0) {
+            int temp = carry;
+            if(posA >= 0) {
+                temp += a.charAt(posA) - '0';
+                posA--;
             }
-            if(pb >= 0) {
-                sum += b.charAt(pb) - '0';
-                pb--;
+            if(posB >= 0) {
+                temp += b.charAt(posB) - '0';
+                posB--;
             }
-            sb.insert(0, sum % 2);
-            carry = sum >= 2 ? 1 : 0;
+            sb.insert(0, temp % 2);
+            carry = temp / 2;
         }
         if(carry == 1) {
             sb.insert(0, 1);
