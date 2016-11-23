11447fdf3c5b144b3b5ee6f46f9e5d9fb017ae51
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-23 13:45:50 -0500

    Update MinimumDepthOfBinaryTree.java

--- easy/MinimumDepthOfBinaryTree.java
+++ easy/MinimumDepthOfBinaryTree.java
@@ -12,64 +12,23 @@ public class Solution1 {
         if(root == null) {
             return 0;
         }
-        return minDepthHelper(root.left, root.right, 1);
+        return minDepthHelper(root);
     }
     
-    public int minDepthHelper(TreeNode l, TreeNode r, int depth) {
-        if(l == null && r == null) {
-            return depth;
-        } else if(l == null && r != null) {
-            return minDepthHelper(r.left, r.right, depth + 1);
-        } else if(l != null && r == null) {
-            return minDepthHelper(l.left, l.right, depth + 1);
+    private int minDepthHelper(TreeNode node) {
+        if(node.left == null && node.right == null) {
+            return 1;
+        } else if(node.left == null && node.right != null) {
+            return minDepthHelper(node.right) + 1;
+        } else if(node.left != null && node.right == null) {
+            return minDepthHelper(node.left) + 1;
         } else {
-            return Math.min(minDepthHelper(l.left, l.right, depth + 1), minDepthHelper(r.left, r.right, depth + 1));
+            return Math.min(minDepthHelper(node.left), minDepthHelper(node.right)) + 1;
         }
     }
 }
 
 public class Solution2 {
-    public int minDepth(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
-        return minDepthHelper(root, 0);
-    }
-    
-    public int minDepthHelper(TreeNode n, int depth) {
-        depth++;
-        if(n == null) {
-            return Integer.MAX_VALUE;
-        }
-        if(n.left == null && n.right == null) {
-            return depth;
-        }
-        return Math.min(minDepthHelper(n.left, depth), minDepthHelper(n.right, depth));
-    }
-}
-
-public class Solution3 {
-    public int minDepth(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
-        return minDepthHelper(root, 1);
-    }
-    
-    public int minDepthHelper(TreeNode node, int depth) {
-        if(node.left == null && node.right == null) {
-            return depth;
-        } else if(node.left != null && node.right == null) {
-            return minDepthHelper(node.left, depth + 1);
-        } else if(node.left == null && node.right != null) {
-            return minDepthHelper(node.right, depth + 1);
-        } else {
-            return Math.min(minDepthHelper(node.left, depth + 1), minDepthHelper(node.right, depth + 1));
-        }
-    }
-}
-
-public class Solution4 {
     public int minDepth(TreeNode root) {
         if(root == null) {
             return 0;
