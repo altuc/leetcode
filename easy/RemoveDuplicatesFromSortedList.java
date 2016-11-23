a5a953f9c89b55bf765c02ccfe2f82cac2f577ae
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-22 22:32:53 -0500

    Update RemoveDuplicatesFromSortedList.java

--- easy/RemoveDuplicatesFromSortedList.java
+++ easy/RemoveDuplicatesFromSortedList.java
@@ -9,25 +9,7 @@
  *     }
  * }
  */
-public class Solution1 {
-    public ListNode deleteDuplicates(ListNode head) {
-        if(head == null || head.next == null) {
-            return head;
-        }
-        ListNode sentinel = new ListNode(0);
-        sentinel.next = head;
-        while(head.next != null) {
-            if(head.val == head.next.val) {
-                head.next = head.next.next;
-            } else {
-                head = head.next;
-            }
-        }
-        return sentinel.next;
-    }
-}
-
-public class Solution2 {
+public class Solution {
     public ListNode deleteDuplicates(ListNode head) {
         if(head == null || head.next == null) {
             return head;
