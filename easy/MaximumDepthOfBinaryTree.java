c9304310b10ba701d26a4a960cf944aee9f16cee
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-23 11:01:50 -0500

    Update MaximumDepthOfBinaryTree.java

--- easy/MaximumDepthOfBinaryTree.java
+++ easy/MaximumDepthOfBinaryTree.java
@@ -7,56 +7,11 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution1 {
+public class Solution {
     public int maxDepth(TreeNode root) {
         if(root == null) {
             return 0;
         }
-        return maxDepthHelper(root.left, root.right, 1);
-    }
-    
-    public int maxDepthHelper(TreeNode l, TreeNode r, int depth) {
-        if(l == null && r == null) {
-            return depth;
-        } else if(l == null && r != null) {
-            return maxDepthHelper(r.left, r.right, depth + 1);
-        } else if(l != null && r == null) {
-            return maxDepthHelper(l.left, l.right, depth + 1);
-        } else {
-            return Math.max(maxDepthHelper(l.left, l.right, depth + 1), maxDepthHelper(r.left, r.right, depth + 1));
-        }
-    }
-}
-
-public class Solution2 {
-    public int maxDepth(TreeNode root) {
-        if(root == null) {
-            return 0;
-        }
-        return maxDepthHelper(root, 0);
-    }
-    
-    public int maxDepthHelper(TreeNode n, int depth) {
-        depth++;
-        if(n == null) {
-            return 0;
-        }
-        if(n.left == null && n.right == null) {
-            return depth;
-        }
-        return Math.max(maxDepthHelper(n.left, depth), maxDepthHelper(n.right, depth));
-    }
-}
-
-public class Solution3 {
-    public int maxDepth(TreeNode root) {
-        return maxDepthHelper(root, 0);
-    }
-    
-    public int maxDepthHelper(TreeNode node, int depth) {
-        if(node == null) {
-            return depth;
-        }
-        return Math.max(maxDepthHelper(node.left, depth + 1), maxDepthHelper(node.right, depth + 1));
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 }
