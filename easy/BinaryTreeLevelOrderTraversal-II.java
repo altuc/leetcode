cd89a81edacd04ccdc2522174b881e36b5e94e3f
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-23 13:57:37 -0500

    Update BinaryTreeLevelOrderTraversal-II.java

--- easy/BinaryTreeLevelOrderTraversal-II.java
+++ easy/BinaryTreeLevelOrderTraversal-II.java
@@ -7,37 +7,7 @@
  *     TreeNode(int x) { val = x; }
  * }
  */
-public class Solution1 {
-    public List<List<Integer>> levelOrderBottom(TreeNode root) {
-        ArrayList<List<Integer>> res = new ArrayList<List<Integer>>();
-        if(root == null) {
-            return res;
-        }
-        Queue<TreeNode> q = new LinkedList<TreeNode>();
-        q.add(root);
-        while(!q.isEmpty()) {
-            ArrayList<Integer> level = new ArrayList<Integer>();
-            ArrayList<TreeNode> nodeList = new ArrayList<TreeNode>();
-            while(!q.isEmpty()) {
-                nodeList.add(q.remove());
-            }
-            for(TreeNode n : nodeList) {
-                level.add(n.val);
-                if(n.left != null) {
-                    q.add(n.left);
-                }
-                if(n.right != null) {
-                    q.add(n.right);
-                }
-            }
-            res.add(level);
-        }
-        Collections.reverse(res);
-        return res;
-    }
-}
-
-public class Solution2 {
+public class Solution {
     public List<List<Integer>> levelOrderBottom(TreeNode root) {
         List<List<Integer>> res = new ArrayList<List<Integer>>();
         if(root == null) {
