57e698534ed74cd60ade9685137fb1ab77a08a54
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-23 21:57:05 -0500

    Update LinkedListCycle.java

--- medium/LinkedListCycle.java
+++ medium/LinkedListCycle.java
@@ -9,7 +9,7 @@
  *     }
  * }
  */
-public class Solution1 {
+public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
         ListNode slow = head;
@@ -23,23 +23,5 @@ public class Solution1 {
                 return true;
             }
         }
-    }
-}
-
-public class Solution2 {
-    public boolean hasCycle(ListNode head) {
-        if(head == null || head.next == null || head.next.next == null) {
-            return false;
-        }
-        ListNode fast = head;
-        ListNode slow = head;
-        while(fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
-            if(slow == fast) {
-                return true;
-            }
-        }
-        return false;
     }
 }
