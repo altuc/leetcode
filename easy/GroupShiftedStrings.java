bc1cfa246df51819b57d0e68822b1ebe1fffd1f3
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-24 18:39:17 -0500

    Update GroupShiftedStrings.java

--- easy/GroupShiftedStrings.java
+++ easy/GroupShiftedStrings.java
@@ -4,21 +4,22 @@ public class Solution {
         if(strings == null || strings.length == 0) {
             return res;
         }
-        HashMap<String, List<String>> map = new HashMap<String, List<String>>();
-        for(String s : strings) {
-            String index = "";
-            for(int i = 1; i < s.length(); i++) {
-                int diff = s.charAt(i) - s.charAt(i - 1) >= 0 ? s.charAt(i) - s.charAt(i - 1) : s.charAt(i) - s.charAt(i - 1) + 26;
-                index += String.valueOf(diff);
+        Map<String, List<String>> map = new HashMap<String, List<String>>();
+        for(String str : strings) {
+            int len = str.length();
+            String key = "";
+            for(int i = 1; i < len; i++) {
+                int diff = str.charAt(i) - str.charAt(i - 1) < 0 ? str.charAt(i) - str.charAt(i - 1) + 26: str.charAt(i) - str.charAt(i - 1);
+                key += diff;
             }
-            if(!map.containsKey(index)) {
-                map.put(index, new ArrayList<String>());
+            if(!map.containsKey(key)) {
+                map.put(key, new ArrayList<String>());
             }
-            map.get(index).add(s);
+            map.get(key).add(str);
         }
-        for(List<String> ls : map.values()) {
-            Collections.sort(ls);
-            res.add(ls);
+        for(List<String> group : map.values()) {
+            Collections.sort(group);
+            res.add(group);
         }
         return res;
     }
