2118a9940adb3497799b6e3635cd27c5d12fbd5e
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-24 21:19:24 -0500

    Update ValidAnagram.java

--- easy/ValidAnagram.java
+++ easy/ValidAnagram.java
@@ -1,32 +1,18 @@
 public class Solution1 {
     public boolean isAnagram(String s, String t) {
-        if(s == null && t == null) {
-            return true;
-        }
-        if((s == null && t != null) || (s != null && t == null)) {
+        if(s == null || t == null || s.length() != t.length()) {
             return false;
         }
-        if(s.length() != t.length()) {
-            return false;
+        int[] arr = new int[26];
+        int len = s.length();
+        for(int i = 0; i < len; i++) {
+            arr[s.charAt(i) - 'a']++;
         }
-        HashMap<Character, Integer> map = new HashMap<Character, Integer>();
-        for(char c : s.toCharArray()) {
-            if(map.containsKey(c)) {
-                map.put(c, map.get(c) + 1);
-            } else {
-                map.put(c, 1);
-            }
-        }
-        for(char c : t.toCharArray()) {
-            if(!map.containsKey(c)) {
+        for(int i = 0; i < len; i++) {
+            if(arr[t.charAt(i) - 'a'] == 0) {
                 return false;
-            } else {
-                if(map.get(c) == 0) {
-                    return false;
-                } else {
-                    map.put(c, map.get(c) - 1);
-                }
             }
+            arr[t.charAt(i) - 'a']--;
         }
         return true;
     }
@@ -34,20 +20,15 @@ public class Solution1 {
 
 public class Solution2 {
     public boolean isAnagram(String s, String t) {
-        if(s == null && t == null) {
-            return true;
-        }
-        if((s == null && t != null) || (s != null && t == null)) {
-            return false;
-        }
-        if(s.length() != t.length()) {
+        if(s == null || t == null || s.length() != t.length()) {
             return false;
         }
         char[] sc = s.toCharArray();
         char[] tc = t.toCharArray();
         Arrays.sort(sc);
         Arrays.sort(tc);
-        for(int i = 0; i < sc.length; i++) {
+        int len = sc.length;
+        for(int i = 0; i < len; i++) {
             if(sc[i] != tc[i]) {
                 return false;
             }
