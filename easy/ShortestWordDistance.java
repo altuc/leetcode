f0bbd13ec25b1b199c63ddd5a014180cba2633d7
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-24 20:59:01 -0500

    Update ShortestWordDistance.java

--- easy/ShortestWordDistance.java
+++ easy/ShortestWordDistance.java
@@ -1,20 +1,17 @@
 public class Solution {
     public int shortestDistance(String[] words, String word1, String word2) {
-        int pos1 = -1;
-        int pos2 = -1;
+        int pos1 = -1, pos2 = -1;
+        int len = words.length;
         int res = Integer.MAX_VALUE;
-        for(int i = 0; i < words.length; i++) {
+        for(int i = 0; i < len; i++) {
             if(words[i].equals(word1)) {
                 pos1 = i;
-                if(pos2 != -1) {
-                    res = Math.min(res, pos1 - pos2);
-                }
             }
             if(words[i].equals(word2)) {
                 pos2 = i;
-                if(pos1 != -1) {
-                    res = Math.min(res, pos2 - pos1);
-                }
+            }
+            if(pos1 != -1 && pos2 != -1) {
+                res = Math.min(res, Math.abs(pos1 - pos2));
             }
         }
         return res;
