a0632b6f5051875bd4bdfac4a38614d59a9de48b
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-25 16:41:55 -0500

    Update FlipGame.java

--- easy/FlipGame.java
+++ easy/FlipGame.java
@@ -1,28 +1,12 @@
-public class Solution1 {
+public class Solution {
     public List<String> generatePossibleNextMoves(String s) {
         List<String> res = new ArrayList<String>();
         if(s == null || s.length() < 2) {
             return res;
         }
-        char[] ca = s.toCharArray();
-        for(int i = 1; i < s.length(); i++) {
-            if(ca[i - 1] == '+' && ca[i] == '+') {
-                ca[i] = '-';
-                ca[i - 1] = '-';
-                res.add(new String(ca));
-                ca[i] = '+';
-                ca[i - 1] = '+';
-            }
-        }
-        return res;
-    }
-}
-
-public class Solution2 {
-    public List<String> generatePossibleNextMoves(String s) {
-        List<String> res = new ArrayList<String>();
-        for(int i = 0; i < s.length() - 1; i++) {
-            if(s.charAt(i) == '+' && s.charAt(i + 1) == '+') {
+        int len = s.length() - 1;
+        for(int i = 0; i < len; i++) {
+            if(s.substring(i, i + 2).equals("++")) {
                 res.add(s.substring(0, i) + "--" + s.substring(i + 2));
             }
         }
