15d1feb239d9e5518f7b581bdaf4c5a1128c915a
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-25 16:23:54 -0500

    Update MoveZeros.java

--- easy/MoveZeros.java
+++ easy/MoveZeros.java
@@ -1,53 +1,15 @@
-public class Solution1 {
+public class Solution {
     public void moveZeroes(int[] nums) {
         if(nums == null || nums.length <= 1) {
             return;
         }
-        int start = 0;
-        int end = 0;
-        while(end < nums.length) {
-            if(nums[end] == 0) {
-                end++;
-            } else {
-                int temp = nums[start];
-                nums[start] = nums[end];
-                nums[end] = temp;
-                start++;
-                end++;
-            }
-        }
-    }
-}
-
-public class Solution2 {
-    public void moveZeroes(int[] nums) {
-        if(nums == null || nums.length <= 1) {
-            return;
-        }
-        int tail = 0;
-        for(int i = 0; i < nums.length; i++) {
-            if(nums[i] != 0) {
-                nums[tail++] = nums[i];
-            }
-        }
-        while(tail < nums.length) {
-            nums[tail++] = 0;
-        }
-    }
-}
-
-public class Solution3 {
-    public void moveZeroes(int[] nums) {
-        if(nums == null || nums.length <= 1) {
-            return;
-        }
-        int firstZero = 0;
-        for(int i = 0; i < nums.length; i++) {
+        int pos = 0, len = nums.length;
+        for(int i = 0; i < len; i++) {
             if(nums[i] != 0) {
                 int temp = nums[i];
-                nums[i] = 0;
-                nums[firstZero] = temp;
-                firstZero++;
+                nums[i] = nums[pos];
+                nums[pos] = temp;
+                pos++;
             }
         }
     }
