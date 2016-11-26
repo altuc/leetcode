457664c836ed5f1a0a3b2fdcf68e7ea172e7e7bf
beeacoder <zhou.hanj@husky.neu.edu>
2016-11-26 18:54:02 -0500

    Update FindTheDifference.java

--- easy/FindTheDifference.java
+++ easy/FindTheDifference.java
@@ -1,7 +1,7 @@
 public class Solution1 {
     public char findTheDifference(String s, String t) {
         char res = t.charAt(t.length() - 1);
-        for(int i = 0; i < s.length(); i++) {
+        for(int i = s.length() - 1; i >= 0; i--) {
             res += t.charAt(i);
             res -= s.charAt(i);
         }
@@ -11,12 +11,10 @@ public class Solution1 {
 
 public class Solution2 {
     public char findTheDifference(String s, String t) {
-        char res = 0;
-        for(char c : s.toCharArray()) {
-            res ^= c;
-        }
-        for(char c : t.toCharArray()) {
-            res ^= c;
+        char res = t.charAt(t.length() - 1);
+        for(int i = s.length() - 1; i >= 0; i--) {
+            res ^= t.charAt(i);
+            res ^= s.charAt(i);
         }
         return res;
     }
