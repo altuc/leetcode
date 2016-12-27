72a87f3bcf4e73e0b5cb68fdff3f979c05fd6bf8
beeacoder <zhou.hanj@husky.neu.edu>
2016-12-27 15:25:44 -0500

    Update AddTwoNumbers.java

--- medium/AddTwoNumbers.java
+++ medium/AddTwoNumbers.java
@@ -9,7 +9,7 @@
  *     }
  * }
  */
-public class Solution1 {
+public class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         if(l1 == null) {
             return l2;
@@ -30,11 +30,7 @@ public class Solution1 {
                 sum += l2.val;
                 l2 = l2.next;
             }
-            if(sum >= 10) {
-                carry = 1;
-            } else {
-                carry = 0;
-            }
+            carry = sum / 10;
             cur.next = new ListNode(sum % 10);
             cur = cur.next;
         }
@@ -42,36 +38,5 @@ public class Solution1 {
             cur.next = new ListNode(1);
         }
         return sentinel.next;
-    }
-}
-
-public class Solution2 {
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-       if(l1 == null) {
-           return l2;
-       }
-       if(l2 == null) {
-           return l1;
-       }
-       ListNode sentinel = new ListNode(0);
-       ListNode current = sentinel;
-       int sum = 0;
-       while(l1 != null || l2 != null) {
-           if(l1 != null) {
-               sum += l1.val;
-               l1 = l1.next;
-           }
-           if(l2 != null) {
-               sum += l2.val;
-               l2 = l2.next;
-           }
-           current.next = new ListNode(sum % 10);
-           current = current.next;
-           sum /= 10;
-           if(sum == 1) {
-               current.next = new ListNode(1);
-           }
-       }
-       return sentinel.next;
     }
 }
