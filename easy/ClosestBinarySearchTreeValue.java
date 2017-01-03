a166bd48982f4fcb2942be0330eb4c6cef5a0e4e
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-02 19:24:02 -0500

    Update ClosestBinarySearchTreeValue.java

--- easy/ClosestBinarySearchTreeValue.java
+++ easy/ClosestBinarySearchTreeValue.java
@@ -9,11 +9,10 @@
  */
 public class Solution {
     public int closestValue(TreeNode root, double target) {
-        TreeNode n = root;
-        int res = n.val;
-        while(n != null) {
-            res = Math.abs(n.val - target) < Math.abs(res - target) ? n.val : res;
-            n = n.val > target ? n.left : n.right;
+        int res = root.val;
+        while(root != null) {
+            res = Math.abs(root.val - target) < Math.abs(res - target) ? root.val : res;
+            root = root.val > target ? root.left : root.right;
         }
         return res;
     }
