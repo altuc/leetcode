4b9141ea4b771051eeb839ffef36c7bb22a5b731
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-03 00:00:04 -0500

    Update MergeTwoSortedLists.java

--- easy/MergeTwoSortedLists.java
+++ easy/MergeTwoSortedLists.java
@@ -11,38 +11,31 @@
  */
 public class Solution1 {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        if(l1 == null){
+        if(l1 == null) {
             return l2;
         }
-        if(l2 == null){
+        if(l2 == null) {
             return l1;
         }
         ListNode sentinel = new ListNode(0);
-        if(l1.val < l2.val) {
-            sentinel.next = l1;
-            l1 = l1.next;
-        } else {
-            sentinel.next = l2;
-            l2 = l2.next;
-        }
-        ListNode p = sentinel.next;
+        ListNode cur = sentinel;
         while(l1 != null || l2 != null) {
             if(l1 != null && l2 != null) {
-                if(l1.val < l2.val) {
-                    p.next = l1;
+                if(l1.val <= l2.val) {
+                    cur.next = l1;
                     l1 = l1.next;
                 } else {
-                    p.next = l2;
+                    cur.next = l2;
                     l2 = l2.next;
                 }
-            } else if (l1 == null) {
-                p.next = l2;
+            } else if(l1 == null) {
+                cur.next = l2;
                 l2 = l2.next;
             } else {
-                p.next = l1;
+                cur.next = l1;
                 l1 = l1.next;
             }
-            p = p.next;
+            cur = cur.next;
         }
         return sentinel.next;
     }
