049725be6220862e66023c975d070f9a66940044
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-03 22:34:51 -0500

    Update FindPeakElement.java

--- medium/FindPeakElement.java
+++ medium/FindPeakElement.java
@@ -1,10 +1,7 @@
 public class Solution1 {
     public int findPeakElement(int[] nums) {
-        if(nums.length == 1) {
-            return 0;
-        }
-        int res = 0;
-        for(int i = 1; i < nums.length; i++) {
+        int res = 0, len = nums.length;
+        for(int i = 1; i < len; i++) {
             if(nums[i] > nums[i - 1]) {
                 res = i;
             }
@@ -15,8 +12,7 @@ public class Solution1 {
 
 public class Solution2 {
     public int findPeakElement(int[] nums) {
-        int start = 0;
-        int end = nums.length - 1;
+        int start = 0, end = nums.length - 1;
         while(start < end) {
             int mid = start + (end - start) / 2;
             if(nums[mid] < nums[mid + 1]) {
