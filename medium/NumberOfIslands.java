64735093809e211dada4cc10403ff5185bf6f8df
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-04 21:54:50 -0500

    Update NumberOfIslands.java

--- medium/NumberOfIslands.java
+++ medium/NumberOfIslands.java
@@ -1,28 +1,28 @@
 public class Solution {
     public int numIslands(char[][] grid) {
-        int count = 0;
         if(grid == null || grid.length == 0 || grid[0].length == 0) {
-            return count;
+            return 0;
         }
-        for(int i = 0; i < grid.length; i++) {
-            for(int j = 0; j < grid[0].length; j++) {
+        int n = grid.length, m = grid[0].length, count = 0;
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < m; j++) {
                 if(grid[i][j] == '1') {
-                    numIslandsHelper(grid, i, j);
                     count++;
+                    numIslandsHelper(grid, i, j);
                 }
             }
         }
         return count;
     }
     
-    public void numIslandsHelper(char[][] grid, int i, int j) {
+    private void numIslandsHelper(char[][] grid, int i, int j) {
         if(i < 0 || i == grid.length || j < 0 || j == grid[0].length || grid[i][j] == '0') {
             return;
         }
         grid[i][j] = '0';
-        numIslandsHelper(grid, i - 1, j);
-        numIslandsHelper(grid, i + 1, j);
-        numIslandsHelper(grid, i, j - 1);
-        numIslandsHelper(grid, i, j + 1);
+        int[][] dirs = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for(int[] dir : dirs) {
+            numIslandsHelper(grid, i + dir[0], j + dir[1]);
+        }
     }
 }
