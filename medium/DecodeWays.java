877131ed305a88430a114e3745801fb7ea097cb3
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-04 21:36:46 -0500

    Update DecodeWays.java

--- medium/DecodeWays.java
+++ medium/DecodeWays.java
@@ -3,28 +3,18 @@ public class Solution {
         if(s == null || s.isEmpty()) {
             return 0;
         }
-        if(s.length() == 1) {
-            return s.charAt(0) == '0' ? 0 : 1;
-        }
-        int n = s.length();
-        int[] res = new int[n];
+        int len = s.length();
+        int[] res = new int[len];
         res[0] = s.charAt(0) == '0' ? 0 : 1;
-        if(s.charAt(0) != '0' && s.charAt(1) != '0') {
-            res[1] += 1;
-        }
-        if(Integer.parseInt(s.substring(0, 2)) >= 10 && Integer.parseInt(s.substring(0, 2)) <= 26) {
-            res[1] += 1;
-        }
-        for(int i = 2; i < n; i++) {
-            int first = Integer.parseInt(s.substring(i, i + 1));
-            int second = Integer.parseInt(s.substring(i - 1, i + 1));
-            if(first >= 1 && first <= 9) {
+        for(int i = 1; i < len; i++) {
+            int num = Integer.parseInt(s.substring(i - 1, i + 1));
+            if(num >= 10 && num <= 26) {
+                res[i] = i == 1 ? 1 : res[i - 2];
+            }
+            if(s.charAt(i) != '0') {
                 res[i] += res[i - 1];
             }
-            if(second >= 10 && second <= 26) {
-                res[i] += res[i - 2];
-            }
         }
-        return res[n - 1];
+        return res[len - 1];
     }
 }
