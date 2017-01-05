9bd8814904b9d952f1d9dc81e245595ba7c3f68a
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-05 16:16:03 -0500

    Update ReverseWordsInAString-II.java

--- medium/ReverseWordsInAString-II.java
+++ medium/ReverseWordsInAString-II.java
@@ -1,25 +1,25 @@
 public class Solution {
     public void reverseWords(char[] s) {
-        if(s == null || s.length <= 2) {
+        if(s == null || s.length < 3) {
             return;
         }
-        reverse(s, 0, s.length - 1);
-        int left = 0;
-        for(int i = 0; i <= s.length; i++) {
-            if(i == s.length || s[i] == ' ') {
-                reverse(s, left, i - 1);
-                left = i + 1;
+        int start = 0, end = s.length;
+        reverse(s, 0, end - 1);
+        for(int i = 0; i <= end; i++) {
+            if(i == end || s[i] == ' ') {
+                reverse(s, start, i - 1);
+                start = i + 1;
             }
         }
     }
     
-    public void reverse(char[] s, int start, int end) {
-        while(start < end) {
-            char temp = s[start];
-            s[start] = s[end];
-            s[end] = temp;
-            start++;
-            end--;
+    private void reverse(char[] s, int i, int j) {
+        while(i < j) {
+            char temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
+            i++;
+            j--;
         }
     }
 }
