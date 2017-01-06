c760f5c92796d243614965542c9a20894ef8cb51
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-05 22:36:50 -0500

    Update MissingNumber.java

--- medium/MissingNumber.java
+++ medium/MissingNumber.java
@@ -1,12 +1,12 @@
 // Sum
 public class Solution1 {
     public int missingNumber(int[] nums) {
-        int res = 0, n = nums.length;
+        int n = nums.length;
+        int res = n;
         for(int i = 0; i < n; i++) {
             res += i;
             res -= nums[i];
         }
-        res += n;
         return res;
     }
 }
