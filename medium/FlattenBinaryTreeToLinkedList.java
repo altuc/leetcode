265b87632bdfcabcd195d34351fc1e171f1171c9
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-06 16:45:49 -0500

    Update FlattenBinaryTreeToLinkedList.java

--- medium/FlattenBinaryTreeToLinkedList.java
+++ medium/FlattenBinaryTreeToLinkedList.java
@@ -55,3 +55,21 @@ public class Solution2 {
         }
     }
 }
+
+public class Solution3 {
+    public void flatten(TreeNode root) {
+        if(root == null) {
+            return;
+        }
+        TreeNode left = root.left;
+        TreeNode right = root.right;
+        flatten(left);
+        flatten(right);
+        root.right = left;
+        root.left = null;
+        while(root.right != null) {
+            root = root.right;
+        }
+        root.right = right;
+    }
+}
