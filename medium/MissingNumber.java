5ad03da05abfd6c4729214d4b67a3705b026e32a
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-05 22:34:33 -0500

    Update MissingNumber.java

--- medium/MissingNumber.java
+++ medium/MissingNumber.java
@@ -1,11 +1,42 @@
-public class Solution {
+// Sum
+public class Solution1 {
     public int missingNumber(int[] nums) {
-        int sum = 0;
-        int total = 0;
-        for(int i = 0; i < nums.length; i++) {
-            sum += nums[i];
-            total += i;
+        int res = 0, n = nums.length;
+        for(int i = 0; i < n; i++) {
+            res += i;
+            res -= nums[i];
         }
-        return total + nums.length - sum;
+        res += n;
+        return res;
+    }
+}
+
+// XOR
+public class Solution2 {
+    public int missingNumber(int[] nums) {
+        int n = nums.length;
+        int res = n;
+        for(int i = 0; i < n; i++) {
+            res ^= i;
+            res ^= nums[i];
+        }
+        return res;
+    }
+}
+
+// Binary search
+public class Solution3 {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        int start = 0, end = nums.length;
+        while(start < end) {
+            int mid = start + (end - start) / 2;
+            if(nums[mid] <= mid) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+        return start;
     }
 }
