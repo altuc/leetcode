98c13cf1617979c23c9adf8680872d5696e77cf8
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-07 17:49:44 -0500

    Update FindMinimumInRotatedSortedArray.java

--- medium/FindMinimumInRotatedSortedArray.java
+++ medium/FindMinimumInRotatedSortedArray.java
@@ -1,16 +1,4 @@
-// O(N)
-public class Solution1 {
-    public int findMin(int[] nums) {
-        int min = Integer.MAX_VALUE;
-        for(int i = 0; i < nums.length; i++) {
-            min = Math.min(min, nums[i]);
-        }
-        return min;
-    }
-}
-
-// Binary Search - O(logN)
-public class Solution2 {
+public class Solution {
     public int findMin(int[] nums) {
         int start = 0;
         int end = nums.length - 1;
@@ -18,10 +6,8 @@ public class Solution2 {
             int mid = start + (end - start) / 2;
             if(nums[mid] < nums[end]) {
                 end = mid;
-            } else if(nums[mid] > nums[end]) {
-                start = mid + 1;
             } else {
-                return nums[mid];
+                start = mid + 1;
             }
         }
         return nums[start];
