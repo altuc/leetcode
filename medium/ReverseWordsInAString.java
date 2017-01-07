085724693ae2b895cb0f6800a2e11741ef4c81f5
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-06 23:52:53 -0500

    Update ReverseWordsInAString.java

--- medium/ReverseWordsInAString.java
+++ medium/ReverseWordsInAString.java
@@ -7,7 +7,7 @@ public class Solution1 {
         StringBuilder sb = new StringBuilder();
         for(int i = strs.length - 1; i >= 0; i--) {
             if(!strs[i].equals("")) {
-                sb.append(strs[i].trim() + " ");
+                sb.append(strs[i] + " ");
             }
         }
         return sb.toString().trim();
