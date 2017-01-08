5372085a7f2c83d1bd9175ee7b3c1cd4386f5b93
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-07 23:59:35 -0500

    Update HouseRobber-II.java

--- medium/HouseRobber-II.java
+++ medium/HouseRobber-II.java
@@ -6,24 +6,17 @@ public class Solution {
         if(nums.length == 1) {
             return nums[0];
         }
-        if(nums.length == 2) {
-            return Math.max(nums[0], nums[1]);
+        int len = nums.length;
+        int[] notRobLast = new int[len];
+        notRobLast[0] = nums[0];
+        notRobLast[1] = Math.max(nums[0], nums[1]);
+        int[] robLast = new int[len];
+        robLast[0] = 0;
+        robLast[1] = nums[1];
+        for(int i = 2; i < len; i++) {
+            notRobLast[i] = Math.max(notRobLast[i - 2] + nums[i], notRobLast[i - 1]);
+            robLast[i] = Math.max(robLast[i - 2] + nums[i], robLast[i - 1]);
         }
-        if(nums.length == 3) {
-            return Math.max(Math.max(nums[0], nums[1]), nums[2]);
-        }
-        int[] rob1 = new int[nums.length - 1];
-        int[] rob2 = new int[nums.length - 1];
-        rob1[0] = nums[0];
-        rob1[1] = Math.max(nums[0], nums[1]);
-        rob2[0] = nums[1];
-        rob2[1] = Math.max(nums[1], nums[2]);
-        for(int i = 2; i < nums.length - 1; i++) {
-            rob1[i] = Math.max(rob1[i - 2] + nums[i], rob1[i - 1]);
-        }
-        for(int i = 3; i < nums.length; i++) {
-            rob2[i - 1] = Math.max(rob2[i - 3] + nums[i], rob2[i - 2]);
-        }
-        return Math.max(rob1[nums.length - 2], rob2[nums.length - 2]);
+        return Math.max(notRobLast[len - 2], robLast[len - 1]);
     }
 }
