73a0c2d0623ef4123d09d4988777f47899bb3b09
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-09 10:09:58 -0500

    Update ProductOfArrayExceptSelf.java

--- medium/ProductOfArrayExceptSelf.java
+++ medium/ProductOfArrayExceptSelf.java
@@ -1,13 +1,14 @@
 public class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] res = new int[nums.length];
+        int len = nums.length;
+        int[] res = new int[len];
         res[0] = 1;
-        for(int i = 1; i < nums.length; i++) {
+        for(int i = 1; i < len; i++) {
             res[i] = res[i - 1] * nums[i - 1];
         }
-        for(int j = nums.length - 1; j > 0; j--) {
-            res[j] = res[j] * res[0];
-            res[0] = res[0] * nums[j];
+        for(int i = len - 1; i > 0; i--) {
+            res[i] = res[i] * res[0];
+            res[0] = res[0] * nums[i];
         }
         return res;
     }
