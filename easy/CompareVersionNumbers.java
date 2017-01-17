a8457b5e7f2fb47c14814ec1e7240a128faa2ddc
beeacoder <zhou.hanj@husky.neu.edu>
2017-01-16 22:09:49 -0500

    Update CompareVersionNumbers.java

--- easy/CompareVersionNumbers.java
+++ easy/CompareVersionNumbers.java
@@ -2,27 +2,16 @@ public class Solution {
     public int compareVersion(String version1, String version2) {
         String[] ver1 = version1.split("\\.");
         String[] ver2 = version2.split("\\.");
-        int i = 0;
-        while(i < ver1.length && i < ver2.length) {
-            if(Integer.parseInt(ver1[i]) > Integer.parseInt(ver2[i])) {
-                return 1;
-            } 
-            if(Integer.parseInt(ver1[i]) < Integer.parseInt(ver2[i])) {
-                return -1;
-            }
-            i++;
-        }
-        while(i < ver1.length) {
-            if(Integer.parseInt(ver1[i]) != 0) {
+        int len = Math.max(ver1.length, ver2.length);
+        for(int i = 0; i < len; i++) {
+            int num1 = i < ver1.length ? Integer.parseInt(ver1[i]) : 0;
+            int num2 = i < ver2.length ? Integer.parseInt(ver2[i]) : 0;
+            if(num1 > num2) {
                 return 1;
             }
-            i++;
-        }
-        while(i < ver2.length) {
-            if(Integer.parseInt(ver2[i]) != 0) {
+            if(num1 < num2) {
                 return -1;
             }
-            i++;
         }
         return 0;
     }
