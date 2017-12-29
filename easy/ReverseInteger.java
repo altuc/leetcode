4fd8b08dfc2f4ec2d514e1b4871000822febfa50
beeacoder <zhou.hanj@husky.neu.edu>
2017-12-28 20:33:20 -0800

    Update ReverseInteger.java

--- easy/ReverseInteger.java
+++ easy/ReverseInteger.java
@@ -1,17 +1,14 @@
 public class Solution {
     public int reverse(int x) {
-        int res = 0;
+        int result = 0;
         while(x != 0) {
             int temp = x % 10;
-            if((res > Integer.MAX_VALUE / 10 || (res == Integer.MAX_VALUE / 10 && Integer.MAX_VALUE % 10 < temp)) && x > 0) {
+            if(result > Integer.MAX_VALUE / 10 || result < Integer.MIN_VALUE / 10) {
                 return 0;
             }
-            if((res < Integer.MIN_VALUE / 10 || (res == Integer.MIN_VALUE / 10 && Integer.MIN_VALUE % 10 > temp)) && x < 0) {
-                return 0;
-            }
-            res = 10 * res + temp;
+            result = result * 10 + temp;
             x /= 10;
         }
-        return res;
+        return result;
     }
 }
